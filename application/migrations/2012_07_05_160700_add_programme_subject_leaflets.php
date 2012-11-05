d3438eaa54877f6758da626661bf93483566a737
conatus <alex@recordsonribs.com>
2012-11-05 14:50:43 +0000

    Move The Leaflet Tables Back In (That I Nuked)
    I ommited these tables as they weren't in the database dump. Grabbed
    them back from a commit prior to nuking them.

---
+++ application/migrations/2012_07_05_160700_add_programme_subject_leaflets.php
@@ -0,0 +1,42 @@
+<?php
+
+class Add_Programme_Subject_Leaflets{
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		
+		Schema::table('programmes', function($table){	
+			$table->string('leaflet_url', 255);
+			$table->text('additional_leaflet_urls');
+		});
+		Schema::table('programmes_revisions', function($table){
+			$table->string('leaflet_url', 255);
+			$table->text('additional_leaflet_urls');
+		});
+
+
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down(){
+
+		Schema::table('programmes', function($table){	
+			$table->drop_column(array('leaflet_url', 'additional_leaflet_urls'));
+		});
+		Schema::table('programmes_revisions', function($table){
+			$table->drop_column(array('leaflet_url', 'additional_leaflet_urls'));
+		});
+
+	}
+
+	
+}
