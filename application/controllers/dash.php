68346ddbc0aec86f2b2c5c2255be0f7453d151e6
conatus <alex@recordsonribs.com>
2012-11-05 16:50:41 +0000

    Remove Authorisation
    Authorisation now has a dedicated controller, auth.

--- application/controllers/dash.php
+++ application/controllers/dash.php
@@ -9,26 +9,5 @@ class Dash_Controller extends Admin_Controller
         $this->data['programmes'] =  Programme::where('year','=',URI::segment(1))->get();
         return View::make('admin.index',$this->data);
     }
-
-    // Login Stuff
-    public function get_login(){
-    	return View::make('admin.login', $this->data);
-    }
-    public function get_logout(){
-    	Auth::logout();
-		return Redirect::to('login');
-    }
-
-    public function post_login(){
-        if (Auth::attempt(array('username'=>Input::get('username'), 'password'=>Input::get('password'))))
-        {
-            if(Session::has('referrer')){
-                return Redirect::to(Session::get('referrer'));
-            }
-            return Redirect::to('/');
-        }else{
-            return Redirect::to('login');
-        }
-    }
     
 }
