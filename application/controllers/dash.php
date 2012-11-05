917ac79fddea6eb8e5fbfa4c90b884cffedf829e
conatus <alex@recordsonribs.com>
2012-11-05 16:50:04 +0000

    Remove Comments

--- application/controllers/dash.php
+++ application/controllers/dash.php
@@ -1,9 +1,7 @@
 <?php
 class Dash_Controller extends Admin_Controller
 {
-    // Restful controllers allow us to prepend get_ or post_ to our function / url names
-    // in order to logically separate the two types of request. Particularly useful
-    // for CRUD systems.
+    
     public $restful = true;
 
     public function get_index()
