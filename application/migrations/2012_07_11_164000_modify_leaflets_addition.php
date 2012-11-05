425dcb8f1d64ca03f21ffea678b79c624229c690
conatus <alex@recordsonribs.com>
2012-11-05 14:57:54 +0000

    Remove Refefences To Subjects

--- application/migrations/2012_07_11_164000_modify_leaflets_addition.php
+++ application/migrations/2012_07_11_164000_modify_leaflets_addition.php
@@ -3,24 +3,14 @@
 class Modify_Leaflets_Addition {
 
 	/**
-	 * Make changes to the database.
+	 * Change leaflets reference in programmes to allow multiple IDs.
 	 *
 	 * @return void
 	 */
 	public function up()
 	{
 
-		Schema::table('subjects', function($table){
-			$table->string('leaflet_ids',255);
-			$table->drop_column(array('leaflet_url', 'additional_leaflet_urls'));
-		});
-
 		Schema::table('programmes', function($table){
-			$table->string('leaflet_ids',255);
-			$table->drop_column(array('leaflet_url', 'additional_leaflet_urls'));
-		});
-
-		Schema::table('subjects_revisions', function($table){
 			$table->string('leaflet_ids',255);
 			$table->drop_column(array('leaflet_url', 'additional_leaflet_urls'));
 		});
@@ -39,20 +29,8 @@ class Modify_Leaflets_Addition {
 	 */
 	public function down()
 	{	
-		
-		Schema::table('subjects', function($table){	
-			$table->drop_column('leaflet_ids');
-			$table->string('leaflet_url', 255);
-			$table->text('additional_leaflet_urls');
-		});
 
 		Schema::table('programmes', function($table){	
-			$table->drop_column('leaflet_ids');
-			$table->string('leaflet_url', 255);
-			$table->text('additional_leaflet_urls');
-		});
-		
-		Schema::table('subjects_revisions', function($table){	
 			$table->drop_column('leaflet_ids');
 			$table->string('leaflet_url', 255);
 			$table->text('additional_leaflet_urls');
