313463469bd682d1b7faebc450df790b08b0ac7c
conatus <alex@recordsonribs.com>
2012-11-05 16:53:15 +0000

    Create Authorisation Filter With Controller
    Anything admin is now behind the authorisation filter.
    
    This authorisation filter is handled in the auth controller.

--- application/routes.php
+++ application/routes.php
@@ -1,59 +1,63 @@
 <?php
 
+/*
+ * Anything admin facing requires authorisation.
+ */
+Route::group(array('before' => 'auth'), function(){
+	// Any page without a root goes to index
+	Route::any('/',function(){
+	       return Redirect::to(date('Y').'/ug');   
+	});
 
-//Index
-Route::any('([0-9]{4})/(ug|pg)', 'dash@index');
+	// Index
+	Route::any('([0-9]{4})/(ug|pg)', 'dash@index');
 
-//user managment
-Route::get('([0-9]{4})/(ug|pg)/roles', 'roles@index');
-Route::get('([0-9]{4})/(ug|pg)/help', 'help@index');
+	// User managment
+	Route::get('([0-9]{4})/(ug|pg)/roles', 'roles@index');
+	Route::get('([0-9]{4})/(ug|pg)/help', 'help@index');
+
+	// Do Globals
+	Route::any('([0-9]{4})/(ug|pg)/globals', 'globals@index');
+	Route::any('([0-9]{4})/(ug|pg)/globals/(:any?)/(:num?)', 'globals@(:3)');
+	Route::get('([0-9]{4})/(ug|pg)/globals/(:num)/(:any)/(:num)', 'globals@(:4)');
+
+	// Do Programmes
+	Route::any('([0-9]{4})/(ug|pg)/programmes', 'programmes@index');
+	Route::any('([0-9]{4})/(ug|pg)/programmes/(:any?)/(:num?)', 'programmes@(:3)');
+	Route::get('([0-9]{4})/(ug|pg)/programmes/(:num)/(:any)/(:num)', 'programmes@(:4)');
+
+	// Access Meta systems
+	Route::any('([0-9]{4})/(ug|pg)/meta/programmes', 'programmes_meta@index');
+	Route::any('([0-9]{4})/(ug|pg)/meta/programmes/(:any?)/(:num?)', 'programmes_meta@(:3)');
+
+	Route::any('([0-9]{4})/(ug|pg)/meta/globals', 'globals_meta@index');
+	Route::any('([0-9]{4})/(ug|pg)/meta/globals/(:any?)/(:num?)', 'globals_meta@(:3)');
+
+
+	// Customised routing for campuses
+	Route::any('([0-9]{4})/(ug|pg)/campuses', 'campuses@index');
+	Route::any('([0-9]{4})/(ug|pg)/campuses/(:any?)/(:num?)', 'campuses@(:3)');
+
+	// Customised routing for schools
+	Route::any('([0-9]{4})/(ug|pg)/schools', 'schools@index');
+	Route::any('([0-9]{4})/(ug|pg)/schools/(:any?)/(:num?)', 'schools@(:3)');
+
+	// Customised routing for faculties
+	Route::any('([0-9]{4})/(ug|pg)/faculties', 'faculties@index');
+	Route::any('([0-9]{4})/(ug|pg)/faculties/(:any?)/(:num?)', 'faculties@(:3)');
+
+	// Customised routing for awards
+	Route::any('([0-9]{4})/(ug|pg)/awards', 'awards@index');
+	Route::any('([0-9]{4})/(ug|pg)/awards/(:any?)/(:num?)', 'awards@(:3)');
+
+	// Customised routing for leaflets
+	Route::any('([0-9]{4})/(ug|pg)/leaflets', 'leaflets@index');
+	Route::any('([0-9]{4})/(ug|pg)/leaflets/(:any?)/(:num?)', 'leaflets@(:3)');
+});
 
 //Login/out
-Route::any('login', 'dash@login');
-Route::any('logout', 'dash@logout');
-
-//Do Globals
-Route::any('([0-9]{4})/(ug|pg)/globals', 'globals@index');
-Route::any('([0-9]{4})/(ug|pg)/globals/(:any?)/(:num?)', 'globals@(:3)');
-Route::get('([0-9]{4})/(ug|pg)/globals/(:num)/(:any)/(:num)', 'globals@(:4)');
-
-//Do Programmes
-Route::any('([0-9]{4})/(ug|pg)/programmes', 'programmes@index');
-Route::any('([0-9]{4})/(ug|pg)/programmes/(:any?)/(:num?)', 'programmes@(:3)');
-Route::get('([0-9]{4})/(ug|pg)/programmes/(:num)/(:any)/(:num)', 'programmes@(:4)');
-
-//Access Meta systems
-Route::any('([0-9]{4})/(ug|pg)/meta/programmes', 'programmes_meta@index');
-Route::any('([0-9]{4})/(ug|pg)/meta/programmes/(:any?)/(:num?)', 'programmes_meta@(:3)');
-
-Route::any('([0-9]{4})/(ug|pg)/meta/globals', 'globals_meta@index');
-Route::any('([0-9]{4})/(ug|pg)/meta/globals/(:any?)/(:num?)', 'globals_meta@(:3)');
-
-
-// Customised routing for campuses
-Route::any('([0-9]{4})/(ug|pg)/campuses', 'campuses@index');
-Route::any('([0-9]{4})/(ug|pg)/campuses/(:any?)/(:num?)', 'campuses@(:3)');
-
-// Customised routing for schools
-Route::any('([0-9]{4})/(ug|pg)/schools', 'schools@index');
-Route::any('([0-9]{4})/(ug|pg)/schools/(:any?)/(:num?)', 'schools@(:3)');
-
-// Customised routing for faculties
-Route::any('([0-9]{4})/(ug|pg)/faculties', 'faculties@index');
-Route::any('([0-9]{4})/(ug|pg)/faculties/(:any?)/(:num?)', 'faculties@(:3)');
-
-// Customised routing for awards
-Route::any('([0-9]{4})/(ug|pg)/awards', 'awards@index');
-Route::any('([0-9]{4})/(ug|pg)/awards/(:any?)/(:num?)', 'awards@(:3)');
-
-// Customised routing for leaflets
-Route::any('([0-9]{4})/(ug|pg)/leaflets', 'leaflets@index');
-Route::any('([0-9]{4})/(ug|pg)/leaflets/(:any?)/(:num?)', 'leaflets@(:3)');
-
-//Any page without a root goes to index
-Route::any('/',function(){
-       return Redirect::to(date('Y').'/ug');   
-});
+Route::any('login', 'auth@login');
+Route::any('logout', 'auth@logout');
 
 Event::listen('404', function()
 {
@@ -73,5 +77,8 @@ Route::filter('csrf', function()
 Route::filter('auth', function()
 {
     Session::put('referrer', URL::current());
-	if (Auth::guest()) return Redirect::to('login');
+
+    if (Auth::guest()) {
+    	return Redirect::to('login');
+    }
 });
