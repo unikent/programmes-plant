037e0bbd3ebfc9d891084231bf927aaab27647ba
conatus <alex@recordsonribs.com>
2012-11-06 13:42:30 +0000

    Add Additional Migrations
    Migrations that were missed from the intial recreation of migrations.

---
+++ application/migrations/2012_11_06_112505_create_module_data.php
@@ -0,0 +1,64 @@
+<?php
+
+class Create_Module_Data {
+
+	/**
+	 * Create 5 Module Data Fields In The Database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Don't repeat yourself.
+		$number = 1;
+
+		while ($number <= 5) {
+			$GLOBALS['field'] = "mod_$number";
+
+			Schema::table('programmes', function($table){
+				$field = $GLOBALS['field'];
+				$table->string($field . "_title");
+				$table->text($field . "_content");
+			});
+
+			Schema::table('programmes_revisions', function($table){
+				$field = $GLOBALS['field'];
+				$table->string($field . "_title");
+				$table->text($field . "_content");
+			});
+
+			$number++;
+		}
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Don't *ever* repeat yourself.
+		$number = 1;
+
+		while ($number <= 5) {
+			$GLOBALS['field'] = "mod_$number";
+
+			Schema::table('programmes', function($table){
+				$field = $GLOBALS['field'];
+				$table->drop_colume($field . "_title");
+				$table->drop_column($field . "_content");
+			});
+
+			Schema::table('programmes_revisions', function($table){
+				$field = $GLOBALS['field'];
+				$table->drop_colume($field . "_title");
+				$table->drop_column($field . "_content");
+			});
+
+			$number++;
+		}
+	}
+
+	
+}
---
+++ application/migrations/2012_11_06_114800_create_publish_unpublish_bits.php
@@ -0,0 +1,37 @@
+<?php
+
+class Create_Publish_Unpublish_Bits {
+
+	/**
+	 * Add publishing and unpublishing bits to the databases.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('programmes', function($table){	
+			$table->integer('live');
+		});
+
+		Schema::table('globals', function($table){	
+			$table->integer('live');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('programmes', function($table){	
+			$table->drop_column('live');
+		});
+
+		Schema::table('globals', function($table){	
+			$table->drop_column('live');
+		});
+	}
+
+}
