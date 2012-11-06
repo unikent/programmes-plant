05aee58d5099859b665d9ed4d16a25cebc266c64
Matthew Bull <bull.matthew@gmail.com>
2012-11-06 15:16:31 +0000

    changed the globals view folder to globalsettings. Renamed the globals controller class to GlobalSettings_Controller, and modified the views and model definitions in the controller to point to globalsettings for the views and GlobalSetting for the model. Also changed some links in the sidebar to go to /globalsettings, along with changes in the routes file.

--- application/models/globalsetting.php
+++ application/models/globalsetting.php
@@ -5,6 +5,6 @@ class GlobalSetting extends Revisionable {
     public static $timestamps = true;
     public $revision = false;
     protected $revision_type = 'globalsetting';
-    protected $revision_table = 'globals_settings_revisions';
+    protected $revision_table = 'globalsettings_revisions';
     
 }
