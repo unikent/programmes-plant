1ead43ad1313700167ed9af8bb7822ee10add1e8
conatus <alex@recordsonribs.com>
2012-11-06 14:23:33 +0000

    Add Migrations For Missed Elements

---
+++ application/migrations/2012_11_06_141412_add_audit_trial.php
@@ -0,0 +1,37 @@
+<?php
+
+class Add_Audit_Trial {
+
+	/**
+	 * Add audit trail to revisions.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('programmes', function($table){
+			$table->string('published_by');
+		});
+
+		Schema::table('programmes_revisions', function($table){
+			$table->integer("published_by");
+ 		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('programmes', function($table){
+			$table->drop_column('published_by');
+		});
+
+		Schema::table('programmes_revisions', function($table){
+			$table->drop_column("published_by");
+ 		});
+	}
+
+}
