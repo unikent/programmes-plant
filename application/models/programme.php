9265cbccdc0a3bf5526bc908afca49fa7a76de85
Matthew Bull <bull.matthew@gmail.com>
2012-11-09 11:24:53 +0000

    Set ProgrammeRevision model name in Programme class
    The ProgrammeRevision model name is needed in the Revisionable class

--- application/models/programme.php
+++ application/models/programme.php
@@ -2,9 +2,8 @@
 class Programme extends Revisionable
 {
      public static $timestamps = true;
-
      public $revision = false;
-
+     protected $revision_model = 'ProgrammeRevision';
      protected $revision_type = 'programme';
      protected $revision_table = 'programmes_revisions';
 }
