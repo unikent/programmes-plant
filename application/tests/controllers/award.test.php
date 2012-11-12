fef39aef2e1dfa0e1847068574efd6d0279874dd
conatus <alex@recordsonribs.com>
2012-11-12 16:05:35 +0000

    Additional Test Case For Awards
    Covers all the get_methods.

--- application/tests/controllers/award.test.php
+++ application/tests/controllers/award.test.php
@@ -10,8 +10,40 @@ class TestAwards_Controller extends ControllerTestCase
 	public static function setUpBeforeClass()
 	{
 		Tests\Helper::migrate();
+
+		// Remove all elements in the awards table.
+		// These are added by the Create_Intial_Awards migration.
+		TestAward::tearDown();
 	}
 
+	/**
+	 * Remove all the awards.
+	 */
+	public function tearDown()
+	{
+		$awards = Award::all();
+
+		foreach ($awards as $award)
+		{
+			$award->delete();
+		}
+	}
+
+	/**
+	 * Populate the awards database with an array.
+	 * 
+	 * @param array $awards An array of awards to add.
+	 */
+	private function populate($awards)
+	{
+		foreach ($awards as $position => $name)
+		{
+			$award = new Award;
+			$award->id = $position;
+			$award->name = $name;
+			$award->save();
+		}
+	}
 	/**
 	 * Tests the output of get_index, which should show the index page.
 	 * 
@@ -23,5 +55,64 @@ class TestAwards_Controller extends ControllerTestCase
 
 		// Check we get the correct response
 		$this->assertEquals('200', $response->foundation->getStatusCode(), 'Getting the awards index does not return a HTTP 200 code.');
+
+		// Add some awards to the database
+		$input = array(1 => 'BA', 2=> 'MA', 3=> 'PhD');
+		$this->populate($input);
+
+		$data = $this->get_data('awards@index');
+
+		// Flatten awards
+		$returned_array = Array();
+
+		foreach ($data['awards'] as $award)
+		{
+			$returned_array[] = $award->name;
+		}
+
+		foreach ($input as $award)
+		{
+			$this->assertTrue(in_array($award, $returned_array), "Awards index did not return inputted awards.");
+		}
+	}
+
+	/**
+	 * Tests output of get_create.
+	 *
+	 * @covers Awards_Controller::get_create
+	 */
+	public function testget_edit()
+	{
+		$input = array(1 => 'BA', 2 => 'MA', 3 => 'PhD');
+		$this->populate($input);
+
+		$response = $this->get('awards@edit', array('2013', 'ug', '1'));
+
+		// Check we get the correct response
+		$this->assertEquals('200', $response->foundation->getStatusCode(), 'Getting the awards edit page does not return a HTTP 200 code.');
+
+		// Check that we get out what we've put in when we request a view.
+		$count = 1;
+		while ($count <= 3)
+		{
+			$data = $this->get_data('awards@edit', array('2013', 'ug', $count));
+
+			$this->assertEquals($input[$count], $data['award']->name);
+
+			$count++;
+		}
+	}
+
+	/**
+	 * Tests output of get_create.
+	 *
+	 * @covers Awards_Controller::get_create
+	 */
+	public function testget_create()
+	{
+		$response = $this->get('awards@create');
+
+		// Check we get the correct response
+		$this->assertEquals('200', $response->foundation->getStatusCode(), 'Getting the awards creation page does not return a HTTP 200 code.');
 	}
 }
