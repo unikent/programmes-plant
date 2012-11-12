7dbe79637f619d2781d32df75227c2e059d54461
conatus <alex@recordsonribs.com>
2012-11-12 16:04:03 +0000

    Clean Request Data On Every Test
    Just in case.

--- application/tests/lib/ControllerTestCase.php
+++ application/tests/lib/ControllerTestCase.php
@@ -20,6 +20,24 @@ abstract class ControllerTestCase extends PHPUnit_Framework_TestCase
 		//Session::load();
 	}
 
+	public function tearDown()
+	{
+		$this->clean_request();
+	}
+
+	/**
+	 * Ensures we don't have problems with dirty requests.
+	 */
+	private function clean_request(){
+        $request = \Laravel\Request::foundation()->request;
+
+        $req_keys = $request->keys();
+       
+        foreach($req_keys as $key){
+            $request->remove($key);
+        }
+    }
+
 	/**
 	 * Call a controller method.
 	 *
