40f9d19ebec439fe01b98e40e554443bea8f0305
conatus <alex@recordsonribs.com>
2012-11-12 18:04:09 +0000

    Major Refactor Of Controllers And Views
    Adding Simple_Admin allos us to remove a huge quantity of controllers
    and views, making our lives very much easier further down the
    maintenance chain.

---
+++ application/views/admin/forms/single-field.php
@@ -0,0 +1,20 @@
+<h1><?php echo ( $create ? 'New ' . Str::title(Str::singular($type)) : 'Edit ' . Str::title(Str::singular($type)) )?></h1>
+<?php echo Messages::get_html()?>
+<?php echo Form::open(URI::segment(1).'/'.URI::segment(2).'/'. $type .'/'.( $create ? 'create' : 'edit' ), 'POST', array('class'=>'form-horizontal'));?>
+<?php if(! $create): ?> 
+<input type="hidden" name="id" value="<?php echo $item->id?>" />
+<?php endif; ?>
+<fieldset>
+  <legend><?php echo Str::title(Str::singular($type)); ?> Details</legend>
+  <div class="control-group">
+    <?php echo Form::label('name', 'Name', array('class'=>'control-label'))?>
+    <div class="controls">
+      <?php echo Form::text('name',  ( Input::old('uname') || $create ? Input::old('name') : $item->name ),array('placeholder'=>'Enter Name...'))?>
+    </div>
+  </div>
+</fieldset>
+<div class="form-actions">
+  <a class="btn" href="<?php echo url(URI::segment(1).'/'.URI::segment(2).'/' )?>">Go Back</a>
+  <input type="submit" class="btn btn-primary" value="<?php echo ($create ? 'Create ' . Str::title(Str::singular($type)): 'Save ' . Str::title(Str::singular($type)))?>" />
+</div>
+<?php echo Form::close()?>
