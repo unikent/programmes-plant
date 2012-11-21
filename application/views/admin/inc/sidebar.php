65ade816dadde36e7f302b3bf39c26900c58c844
conatus <alex@recordsonribs.com>
2012-11-21 16:58:20 +0000

    Remove Changes

--- application/views/admin/inc/sidebar.php
+++ application/views/admin/inc/sidebar.php
@@ -15,12 +15,6 @@
         <li class="<?php echo ( URI::segment(3) == 'programmesettings' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'programmesettings')?>"><i class="icon-book"></i> Programme settings</a></li>
        <li class="<?php echo ( URI::segment(3) == 'programmes' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'programmes')?>"><i class="icon-book"></i> Programmes</a></li>
           
-
-        <li class="nav-header">User</li>
-        <li class="<?php echo ( URI::segment(2) == '12' ? 'active' : false )?>"><a href="<?php echo url('demo/12')?>"><i class="icon-user"></i> My change requests</a></li>
-        
-
-
         <li class="nav-header">Data</li>
         <li class="<?php echo ( URI::segment(3) == 'campuses' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'campuses')?>"><i class="icon-list-alt"></i> Campuses</a></li>
         <li class="<?php echo ( URI::segment(3) == 'faculties' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'faculties')?>"><i class="icon-list-alt"></i> Faculties</a></li>
@@ -28,12 +22,8 @@
 		    <li class="<?php echo ( URI::segment(3) == 'awards' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'awards')?>"><i class="icon-list-alt"></i> Awards</a></li>
         <li class="<?php echo ( URI::segment(3) == 'leaflets' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'leaflets')?>"><i class="icon-list-alt"></i> Leaflets</a></li>
 		
-     
-
-
+    
        <li class="nav-header">Admin</li>
-       <li class="<?php echo ( URI::segment(1) == 'changes' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'changes/')?>"><i class="icon-user"></i> Change requests</a></li>
-        
        <li class="<?php echo ( URI::segment(1) == 'roles' ? 'active' : false )?>"><a href="<?php echo url($mainpath.'roles')?>"><i class="icon-user"></i> User managment</a></li>
         
      <li class="nav-header">Field Configuration</li>
