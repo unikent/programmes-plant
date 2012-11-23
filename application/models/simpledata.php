723564683c90171023ce6ce4a8365060c388a6c2
conatus <alex@recordsonribs.com>
2012-11-23 10:35:33 +0000

    Correct Documentation

--- application/models/simpledata.php
+++ application/models/simpledata.php
@@ -3,7 +3,7 @@ class SimpleData extends Eloquent
 {
 
     /**
-	 * Gives us the model types as a array, for use in a <option> tag.
+	 * Gives us all the items in this model as a flat id => name array, for use in a <option> tag.
 	 * 
 	 * @return array $options An array where the record ID maps onto the record name.
 	 */
