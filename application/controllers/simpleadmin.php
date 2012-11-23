7638d1a35904b5e9f61b48be4fb931c84d120766
conatus <alex@recordsonribs.com>
2012-11-23 11:28:41 +0000

    Move Simple_Admin_Controller To Eloquent And Move Validation To Model
    This will require downstream changes to inherited classes as
    $this->model no longer contains an instance of the model, but is simply
    the name of the model.
    
    We want to call the model statically in the correct context
    (validation) and non-statically when we are actually doing
    manipulation. This change make the code correspond to this.

--- application/controllers/simpleadmin.php
+++ application/controllers/simpleadmin.php
@@ -12,11 +12,7 @@ class Simple_Admin_Controller extends Admin_Controller {
     var $custom_form = false;
 
     public function __construct()
-    {  
-    	if ($this->model) {
-    		$this->model = new $this->model;
-    	}
-
+    {
         // Quick use variable for access to language files
         $this->l = $this->views . '.';
 
@@ -24,21 +20,35 @@ class Simple_Admin_Controller extends Admin_Controller {
     	parent::__construct();
     }
 
+    /**
+     * Return all data and send to an index view.
+     */
     public function get_index()
     {
-    	$this->data['items'] = $this->model->order_by('id','asc')->get();
+        $model = $this->model;
+
+        $this->data['items'] = $model::order_by('id', 'asc')->all()->get();
 
         $this->layout->nest('content', 'admin.indexes.simple-index', $this->data);
     }
     
     public function get_edit($year, $type, $object_id = false)
     {
-    	// Do our checks to make sure things are in place
-    	if(!$object_id) return Redirect::to($this->views);
+    	if (! $object_id)
+        {
+            Message::add('error', "No " . Str::lower($this->model) . "ID provided, so could not be loaded.");
+            return Redirect::to($this->views);
+        }
 
-    	$object = $this->model->find($object_id);
+        $this->model = $model;
 
-    	if(!$object) return Redirect::to($this->views);
+    	$object = $model::find($object_id);
+
+    	if (! $object)
+        {
+            Message::add('error', __($this->l . 'error.not_found'));
+            return Redirect::to($this->views);
+        }
 
     	$this->data['item'] = $object;
         
@@ -66,83 +76,85 @@ class Simple_Admin_Controller extends Admin_Controller {
         }
     }
 
+    /**
+     * Route for deletion of an item.
+     */
     public function post_delete()
     {
+        $model = $this->model;
+
         $rules = array(
             'id'  => 'required|exists:' . $this->views,
         );
 
-        $validation = Validator::make(Input::all(), $rules);
-
-        if ($validation->fails())
+        if (! $model::is_valid(null, $rules))
         {
             Message::add('error', __($this->l . 'error.delete'));
 
-            return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
+            return Redirect::to(URI::segment(1) . '/' . URI::segment(2) . '/' . $this->views);
         }
         else
         {
-            $remove = $this->model->find(Input::get('id'));
+            $remove = $model::find(Input::get('id'));
             $remove->delete();
 
             Messages::add('success', __($this->l . 'success.delete'));
-            return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
+            return Redirect::to(URI::segment(1) . '/' . URI::segment(2) . '/' . $this->views);
         }
     }
 
+    /**
+     * Create a new item.
+     */
     public function post_create()
     {
+        $model = $this->model;
+
         $rules = array(
             'name'  => 'required|unique:' . $this->views . '|max:255',
         );
 
-        $validation = Validator::make(Input::all(), $rules);
-
-        if ($validation->fails())
+        if (! $model::is_valid(null, $rules))
         {
-            Messages::add('error',$validation->errors->all());
+            Messages::add('error', $model::$validation->errors->all());
 
             return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'/create')->with_input();
         }
-        else
-        {
-            $new = new $this->model;
-            $new->name = Input::get('name');
 
-            $new->save();
- 
-            Messages::add('success', __($this->l . 'success.create'));
+        $new = new $this->model;
 
-            return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
-        }
+        $new->name = Input::get('name');
+        $new->save();
+
+        Messages::add('success', __($this->l . 'success.create'));
+
+        return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
     }
 
     public function post_edit()
     {
+        $model = $this->model;
+        
         $rules = array(
             'id'  => 'required|exists:'. $this->views .',id',
             'name'  => 'required|max:255|unique:'. $this->views . ',name,'.Input::get('id'),
         );
 
-        $validation = Validator::make(Input::all(), $rules);
-
-        if ($validation->fails())
+        if (! $model::is_valid())
         {
-            Messages::add('error',$validation->errors->all());
+            Messages::add('error', $model::$validation->errors->all());
             return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'/edit/'.Input::get('id'));
         }
-        else
-        {
-            $update = $this->model->find(Input::get('id'));
-   
-            $update->name = Input::get('name');
 
-            $update->save();
+        $update = $model::find(Input::get('id'));
 
-            Messages::add('success', __($this->l . 'success.edit'));
+        $update->name = Input::get('name');
 
-            return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
-        }
+        $update->save();
+
+        Messages::add('success', __($this->l . 'success.edit'));
+
+        return Redirect::to(URI::segment(1).'/'.URI::segment(2).'/'.$this->views.'');
     }
 
 }
