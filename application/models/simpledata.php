cd4a10dbbf84e911e2ebd8e39a1dc12e67250671
conatus <alex@recordsonribs.com>
2012-11-23 11:26:42 +0000

    Extend SimpleData To Abstract Validation
    To have the thin controllers, thick models paradigm, this pushes
    validation onto the model, not the controller.

--- application/models/simpledata.php
+++ application/models/simpledata.php
@@ -2,6 +2,43 @@
 class SimpleData extends Eloquent
 {
 
+	/**
+	 * Validation object once it has been created.
+	 */
+	public static $validation = null;
+
+	/**
+	 * The rules for validation in standard Laravel validation arrays.
+	 */
+	public static $rules = array(
+		'title'  => 'required|max:255',
+		'type' => 'in:text,textarea,select,checkbox,help'
+	);
+
+	/**
+	 * Validates input for Field.
+	 * 
+	 * @param array $input The input in Laravel input format.
+	 * @param array $rules An array of Laravel validations which will overwrite the defaults for the class.
+	 * @return $validaton The Laravel validation object.
+	 */
+	public static function is_valid($input = null, $rules = null)
+	{
+		if (is_null($rules))
+		{
+			$rules = static::$rules;
+		}
+
+		if (is_null($input))
+		{
+			$input = Input::all();
+		}
+
+        static::$validation = Validator::make($input, $rules);
+
+        return static::$validation->passes();
+	}
+	
     /**
 	 * Gives us all the items in this model as a flat id => name array, for use in a <option> tag.
 	 * 
