32ebc3362e9ca251f4cccd3de0f4525b053892fe
conatus <alex@recordsonribs.com>
2012-11-25 20:39:06 +0000

    Add Tests For is_valid Behaviours

--- application/tests/models/simpledata.test.php
+++ application/tests/models/simpledata.test.php
@@ -26,9 +26,23 @@ class TestSimpleData extends PHPUnit_Framework_TestCase
 		$this->assertEquals('max:255', Thing::$rules['id']);
 	}
 	
-	public function testis_validReturnsTrueWhenValidationSucceeds() {}
+	public function testis_validReturnsTrueWhenValidationSucceeds() 
+	{
+		Thing::$rules = array('email' => 'required|email', 'address' => 'required|url');
 
-	public function testis_validReturnsFalseWhenValidationFails() {}
+		Request::foundation()->request->add(array('email' => 'alex@example.com', 'address' => 'http://example.com'));
+
+		$this->assertTrue(Thing::is_valid());
+	}
+
+	public function testis_validReturnsFalseWhenValidationFails()
+	{
+		Thing::$rules = array('id' => 'required', );
+
+		Request::foundation()->request->add(array('id' => null));
+
+		$this->assertFalse(Thing::is_valid());
+	}
 
 	public function testpopulate_from_inputSetsModelFromInput() {}
 
