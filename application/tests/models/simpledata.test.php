073d1aa3d2fdd7108fbd0e8b39a0a9f6aeb34041
conatus <alex@recordsonribs.com>
2012-11-25 19:45:54 +0000

    Test Dummies

---
+++ application/tests/models/simpledata.test.php
@@ -0,0 +1,21 @@
+<?php
+
+class TestSimpleData extends PHPUnit_Framework_TestCase
+{
+	public function testall_as_listReturnsArray() {}
+
+	public function testall_as_listReturnsData() {}
+
+	public function testis_validRulesAreInheritedFromParentClass() {}
+
+	public function testis_validRulesAreAddedSuccessfully() {}
+
+	public function testis_validReturnsTrueWhenValidationSucceeds() {}
+
+	public function testis_validReturnsFalseWhenValidationFails() {}
+
+	public function testpopulate_from_inputSetsModelFromInput() {}
+
+	public function testpopulate_from_inputWarnsWhenThereIsNoValidation() {}
+
+}
