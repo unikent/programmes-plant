ac95b6f780348da33811e366f606da35e44b066a
conatus <alex@recordsonribs.com>
2012-11-25 18:33:18 +0000

    Move Input From Controller To Model

--- application/models/school.php
+++ application/models/school.php
@@ -2,4 +2,15 @@
 class School extends SimpleData
 {
 
+	public static $rules = array(
+		'name'  => 'required|unique:schools|max:255',
+		'faculty'  => 'required|exists:faculties,id'
+	);
+
+	public function input()
+	{
+		$this->name = Input::get('name');
+        $this->faculties_id = Input::get('faculty');
+	}
+	
 }
