2926aafab280d9c04813575968d88783ae4d1de3
conatus <alex@recordsonribs.com>
2012-11-25 18:20:17 +0000

    Tests For Schools

---
+++ application/tests/models/school.test.php
@@ -0,0 +1,120 @@
+<?php
+
+class TestSchool extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Set up the database.
+	 */
+	public static function setUpBeforeClass()
+	{
+		Tests\Helper::migrate();
+
+		$campus = new Faculty;
+		$campus->save();
+	}
+
+	/**
+	 * Remove everything in the database.
+	 */
+	public function tearDown()
+	{
+		$schools = School::all();
+
+		foreach ($schools as $school)
+		{
+			$school->delete();
+		}
+	}
+
+	public function testInputPopulatesSchoolObjectSuccessfully()
+	{
+		$input = array('name' => 'Blah', 'faculty' => 1);
+		Request::foundation()->request->add($input);
+
+		$school = new School;
+		$school->input();
+
+		// We should be saving $_POST['faculty'] to $school->faculties_id
+		$input['faculties_id'] = $input['faculty'];
+		unset($input['faculty']);
+		
+		$this->assertEquals($input, $school->attributes);
+	}
+
+	public function testSchoolObjectSavesSuccessfully()
+	{
+		$input = array('name' => 'A School', 'faculties_id' => 1);
+
+		// Handle closure
+		$GLOBALS['input'] = $input;
+
+		$school = new School;
+		foreach($input as $key => $value){ $school->$key = $value; }
+		$school->save();
+
+		$return = DB::table('schools')
+				  ->where(function($query)
+				  {
+				  	$input = $GLOBALS['input'];
+				  	$query->where('name', '=', $input['name']);
+				  	$query->where('faculties_id', '=', $input['faculties_id']);
+				  })
+				  ->get();
+		unset($GLOBALS['input']);
+
+		$this->assertNotEmpty($return, "We didn't write our school to the database - it wasn't there when we tried to get it back.");
+		$this->assertCount(1, $return, "We got more than one school back from the database.");
+
+		foreach($input as $key => $value)
+		{
+			$this->assertEquals($value, $return[0][$key], "Inputted $key => $value came back wrongly.");
+		}
+	}
+
+	public function testValidatesSuccessfullyWhenEverythingIsRight() 
+	{
+		Request::foundation()->request->add(array('name' => 'blah', 'faculty' => 1));
+
+		$this->assertTrue(School::is_valid());
+	}
+
+	public function testFailsToValidateWhenNameIsNotPresent() 
+	{
+		Request::foundation()->request->add(array('name' => null, 'faculty' => 1));
+
+		$this->assertFalse(School::is_valid());
+	}
+
+	public function testFailsToValidateWhenNameIsLongerThan255Characters() 
+	{
+		Request::foundation()->request->add(array('name' => str_pad('', 260, 'a'), 'faculty' => 1));
+
+		$this->assertFalse(School::is_valid());
+	}
+
+	public function testFailsToValidateWhenNameIsNotUnique()
+	{
+		$school = new School;
+		$school->name = 'Fun';
+		$school->save();
+
+		Request::foundation()->request->add(array('name' => 'Fun', 'faculty' => 1));
+
+		$this->assertFalse(School::is_valid());
+	}
+
+	public function testFailsToValidateWhenFacultyIsNotPresent() 
+	{
+		Request::foundation()->request->add(array('name' => 'Blah', 'faculty' => null));
+
+		$this->assertFalse(School::is_valid());
+	}
+
+	public function testFailsToValidateWhenFacultyDoesNotExist()
+	{
+		Request::foundation()->request->add(array('name' => 'Blah', 'faculty' => 2));
+
+		$this->assertFalse(School::is_valid());
+	}
+}
