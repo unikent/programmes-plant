baa470fdabff077376babb1515f15d672aa98cae
conatus <alex@recordsonribs.com>
2012-11-26 13:29:46 +0000

    Pass Tests

--- application/models/campus.php
+++ application/models/campus.php
@@ -2,6 +2,14 @@
 class Campus extends SimpleData
 {
 	public static $rules = array(
-    	'name' => 'required|unique:leaflets|max:255',
+    	'name' => 'required|unique:campuses|max:255',
+    	'identifier' => 'numeric',
+    	'address_1' => 'required',
+    	'address_2' => 'required',
+    	'address_3' => 'required',
+    	'email' => 'email',
+    	'phone' => 'required|match:/^([0-9 ]+)$/',
+    	'postcode' => 'required',
+    	'url' => 'url'
     );
 }
