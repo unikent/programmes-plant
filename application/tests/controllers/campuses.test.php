b1da7a348b645b6a6f63c19a8974c53f572b222f
conatus <alex@recordsonribs.com>
2012-11-26 16:08:08 +0000

    Tests For Campus Create Route

--- application/tests/controllers/campuses.test.php
+++ application/tests/controllers/campuses.test.php
@@ -12,4 +12,45 @@ class TestCampuses_Controller extends ControllerTestCase
 		Tests\Helper::migrate();
 	}
 
+	public function get_create_page()
+	{
+		return $this->get_html('campuses@create');
+	}
+
+	public function testCreateFormIsCorrect()
+	{
+		$html = $this->get_create_page();
+
+		$fields = array(
+			'description' => 'textarea',
+			'identifier' => 'input',
+			'title' => 'input',
+			'address_1' => 'input',
+			'address_2' => 'input',
+			'address_3' => 'input',
+			'town' => 'input',
+			'email' => 'input',
+			'phone' => 'input',
+			'postcode' => 'input',
+			'url' => 'input'
+		);
+
+		foreach($fields as $field => $tag)
+		{
+			$matcher = array(
+			'tag' => $tag,
+			'attributes' => array('name' => $field)
+			);
+
+			$this->assertTag($matcher, $html, "$field (form element type $tag) is missing from the create form");
+
+			$matcher = array(
+				'tag' => 'label',
+				'attributes' => array('for' => $field)
+			);
+
+			$this->assertTag($matcher, $html, "$field (form element type $tag) is missing from the create form");
+		}
+	}
+
 }
