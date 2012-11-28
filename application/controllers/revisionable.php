6d61d25376e738202de9758ac5591fe943a7369e
thybag <thybag@gmail.com>
2012-11-28 09:10:24 +0000

    Tidy documentation a little

--- application/controllers/revisionable.php
+++ application/controllers/revisionable.php
@@ -3,7 +3,14 @@
 class Revisionable_Controller extends Admin_Controller
 {
 
-	
+	 /**
+     * Ensure valid revisionable_item & revision have been provided. Return false if not.
+     *
+     * @param int    $revisionable_item_id  ID of the object with revisions.
+     * @param int    $revision_id The id of the specific revision an action is being taken on
+	 *
+     * @return false | array(revisonble Object, revision object)
+     */
 	private function get_revision_data($revisionable_item_id, $revision_id){
 
 		//Handle global settings & programesettings not having a revisionable_item_id (is always 1)
@@ -30,9 +37,18 @@ class Revisionable_Controller extends Admin_Controller
     }
 
 
+
     /**
      * Routing for GET /$year/$type/$object_id/revert_to_revision/$revision_id
      * Routing for GET /$year/$type/revert_to_revision/$revision_id
+     *
+     * @param int    $year         The year of the programme (not used, but to keep routing happy).
+     * @param string $type         The type, either undegrad/postgrade (not used, but to keep routing happy)
+     * @param int    $revisionable_item_id  The object ID we are reverting to a revision on.
+     * @param int    $revision_id  The revision ID we are reverting to.
+     *
+     * @note revisionable_item_id id can be ommited, resulting in that varible containg 
+     *	the revision id instead. (Object assumed to have id 1 in this case)
      */
     public function get_revert_to_revision($year, $type, $revisionable_item_id = false, $revision_id = false){
         
@@ -51,9 +67,17 @@ class Revisionable_Controller extends Admin_Controller
     }
 
 
-    /**
+     /**
      * Routing for GET /$year/$type/$object_id/make_live/$revision_id
      * Routing for GET /$year/$type/make_live/$revision_id
+     *
+     * @param int    $year         The year of the programme (not used, but to keep routing happy).
+     * @param string $type         The type, either undegrad/postgrade (not used, but to keep routing happy)
+     * @param int    $revisionable_item_id  The object ID we are pushing a revision live on.
+     * @param int    $revision_id  The revision ID we are putting live.
+     *
+     * @note revisionable_item_id id can be ommited, resulting in that varible containg 
+     *	the revision id instead. (Object assumed to have id 1 in this case)
      */
 	public function get_make_live($year, $type, $revisionable_item_id = false, $revision_id = false){
 
@@ -73,9 +97,5 @@ class Revisionable_Controller extends Admin_Controller
         Messages::add('success', "The selected revision has been made live.");
         return Redirect::to($year.'/'.$type.'/'.$this->views.'/edit/'.$item->id);
     }
-    
-
-
-
 
 }
