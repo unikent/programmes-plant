9d84b40000de4134237a3cd6d3bbcf393ed0d911
Matthew Bull <bull.matthew@gmail.com>
2012-11-28 15:19:23 +0000

    Fixed bug with schools form view

--- application/views/admin/schools/form.php
+++ application/views/admin/schools/form.php
@@ -1,21 +1,21 @@
 <h1><?php echo ( $create ? 'New School' : 'Edit School' )?></h1>
 <?php echo Messages::get_html()?>
 <?php echo Form::open(URI::segment(1).'/'.URI::segment(2).'/schools/'.( $create ? 'create' : 'edit' ), 'POST', array('class'=>'form-horizontal'));?>
-<?php if(!$create): ?> <input type="hidden" name="id" value="<?php echo $school->id?>" /> <?php endif; ?>
+<?php if(!$create): ?> <input type="hidden" name="id" value="<?php echo $item->id?>" /> <?php endif; ?>
 <fieldset>
   <legend>School Details</legend>
 
   <div class="control-group">
     <?php echo Form::label(__('schools.name'), 'Name', array('class'=>'control-label'))?>
     <div class="controls">
-      <?php echo Form::text('name',  ( Input::old('uname') || $create ? Input::old('name') : $school->name ),array('placeholder'=>__('schools.name_placeholder')))?>
+      <?php echo Form::text('name',  ( Input::old('uname') || $create ? Input::old('name') : $item->name ),array('placeholder'=>__('schools.name_placeholder')))?>
     </div>
   </div>
 
   <div class="control-group">
     <?php echo Form::label('faculty', __('schools.faculty'), array('class'=>'control-label'))?>
     <div class="controls">
-      <?php echo Form::select('faculty', Faculty::all_as_list(), ($create ? "" : $school->faculties_id ))?>
+      <?php echo Form::select('faculty', Faculty::all_as_list(), ($create ? "" : $item->faculties_id ))?>
     </div>
   </div>
 
