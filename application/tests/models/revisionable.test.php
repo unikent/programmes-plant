94b24135ca5a6ee2a68ca4cbf847fa954da2a632
conatus <alex@recordsonribs.com>
2012-12-05 15:43:53 +0000

    Use A Child Of Revisionable Which Does Implement get_title_field

--- application/tests/models/revisionable.test.php
+++ application/tests/models/revisionable.test.php
@@ -17,7 +17,7 @@ class TestRevisionable extends ModelTestCase
 		Cache::flush();
 
 		// Delete from the database.
-		$programme_settings = ProgrammeSetting::all();
+		$programme_settings = Programme::all();
 
 		foreach ($programme_settings as $programme_setting)
 		{
@@ -26,12 +26,12 @@ class TestRevisionable extends ModelTestCase
 
 		// Kill the list cache (just in case).
 		// This caches everything in memory.
-		ProgrammeSetting::$list_cache = false;
+		Programme::$list_cache = false;
 		
 		parent::tearDown();
 	}
 
-	public function populate($model = 'ProgrammeSetting', $input = false)
+	public function populate($model = 'Programme', $input = false)
 	{
 		if (! $input)
 		{
@@ -44,14 +44,14 @@ class TestRevisionable extends ModelTestCase
 
 	public function testall_as_listReturnsEmptyArrayWhenWeDontHaveAnything()
 	{
-		$this->assertCount(0, ProgrammeSetting::all_as_list());
+		$this->assertCount(0, Programme::all_as_list());
 	}
 
 	public function testall_as_listReturnsAnArrayOfItemsFromDatabase() 
 	{
 		$this->populate();
 
-		$result = ProgrammeSetting::all_as_list();
+		$result = Programme::all_as_list();
 
 		$this->assertTrue(is_array($result));
 		$this->assertEquals(array('1' => 'Thing'), $result);
@@ -62,12 +62,12 @@ class TestRevisionable extends ModelTestCase
 		$this->populate();
 
 		// Warm up the cache.
-		$before_cache = ProgrammeSetting::all_as_list();
+		$before_cache = Programme::all_as_list();
 
 		// Wipe memory cache
-		ProgrammeSetting::$list_cache = false;
+		Programme::$list_cache = false;
 
-		$after_cache = ProgrammeSetting::all_as_list();
+		$after_cache = Programme::all_as_list();
 
 		$this->assertEquals($before_cache, $after_cache);
 	}
@@ -75,12 +75,12 @@ class TestRevisionable extends ModelTestCase
 	public function testall_as_listReturnsTheSameWhenWhenItIsInMemoryCache() 
 	{
 		// Warm up the cache.
-		$before_cache = ProgrammeSetting::all_as_list();
+		$before_cache = Programme::all_as_list();
 
 		// Wipe disk cache
-		Cache::forget('ProgrammeSetting--options-list');
+		Cache::forget('Programme--options-list');
 
-		$after_cache = ProgrammeSetting::all_as_list();
+		$after_cache = Programme::all_as_list();
 
 		$this->assertEquals($before_cache, $after_cache);
 	}
@@ -89,18 +89,18 @@ class TestRevisionable extends ModelTestCase
 	{
 		$this->populate();
 
-		ProgrammeSetting::all_as_list();
+		Programme::all_as_list();
 
-		$this->assertTrue(Cache::has('ProgrammeSetting--options-list'));
+		$this->assertTrue(Cache::has('Programme--options-list'));
 	}
 
 	public function testall_as_listResultsCacheToMemoryWhenThereIsNoCache() {
 		$this->populate();
 
-		ProgrammeSetting::all_as_list();
+		Programme::all_as_list();
 
 		// Check we only have one element here.
-		$this->assertCount(1, ProgrammeSetting::$list_cache['ProgrammeSetting']);
+		$this->assertCount(1, Programme::$list_cache['Programme']);
 	}
 
 	public function testall_as_listIfWeRemoveTheCacheThenWeCanStillGetList()
@@ -108,26 +108,26 @@ class TestRevisionable extends ModelTestCase
 		$this->populate();
 
 		// Warm up cache.
-		$result = ProgrammeSetting::all_as_list();
+		$result = Programme::all_as_list();
 
 		// Remove the cache
 		// This would be run, for example, when we save something.
-		Cache::forget('ProgrammeSetting--options-list');
+		Cache::forget('Programme--options-list');
 
 		// Check we actually forgot it.
-		$this->assertFalse(Cache::has('ProgrammeSetting--options-list'), 'Cache has not been forgotten');
+		$this->assertFalse(Cache::has('Programme--options-list'), 'Cache has not been forgotten');
 
 		// Everything should still work, even if we haven't cached.
-		$this->assertEquals($result, ProgrammeSetting::all_as_list());
+		$this->assertEquals($result, Programme::all_as_list());
 	}
 
 	public function testall_as_listResultsCacheInMemoryAsWellAsOnDisk()
 	{
 		$this->populate();
 
-		ProgrammeSetting::all_as_list();
+		Programme::all_as_list();
 
-		$this->assertEquals(array('1' => 'Thing'), ProgrammeSetting::$list_cache['ProgrammeSetting']);
+		$this->assertEquals(array('1' => 'Thing'), Programme::$list_cache['Programme']);
 	}
 
 	public function testResultsComeFromInMemoryCacheIfItExistsNotFromDisk()
@@ -135,39 +135,39 @@ class TestRevisionable extends ModelTestCase
 		$this->populate();
 
 		// Warm cache, presumably also the in memory cache.
-		$result = ProgrammeSetting::all_as_list();
+		$result = Programme::all_as_list();
 
 		// Add a false cache to the object
 		$false_cache = array('1' => 'Other Thing');
-		ProgrammeSetting::$list_cache['ProgrammeSetting'] = $false_cache;
+		Programme::$list_cache['Programme'] = $false_cache;
 
 		// Remove the disk cache
-		Cache::forget('ProgrammeSetting--options-list');
+		Cache::forget('Programme--options-list');
 
 		// If we get this false cache out, we know we are hitting the in memory, not the file cache.
-		$this->assertEquals($false_cache, ProgrammeSetting::all_as_list());
+		$this->assertEquals($false_cache, Programme::all_as_list());
 	}
 
 	public function testall_as_listResultsComeFromDiskCacheWhenCacheIsWarmedUp() 
 	{
 		// Artifically disk cache something.
 		$false_cache = array('1' => 'Other Thing');
-		Cache::forever('ProgrammeSetting--options-list', $false_cache);
+		Cache::forever('Programme--options-list', $false_cache);
 
 		// We now have nothing in the database, but a cache object.
 		// If we get something back then we aren't hitting the database at all.
-		$this->assertEquals($false_cache, ProgrammeSetting::all_as_list());
+		$this->assertEquals($false_cache, Programme::all_as_list());
 	}
 
 	public function testall_as_listResultsComeFromMemoryCacheWhenCacheIsWarmedUp()
 	{
 		// Artifically create a memory cache something.
 		$false_cache = array('1' => 'Other Thing');
-		ProgrammeSetting::$list_cache['ProgrammeSetting'] = $false_cache;
+		Programme::$list_cache['Programme'] = $false_cache;
 
 		// We have only an in memory cache, no disk and no database at all.
 		// If we get something back, we are getting it from the memory cache.
-		$this->assertEquals($false_cache, ProgrammeSetting::all_as_list());
+		$this->assertEquals($false_cache, Programme::all_as_list());
 	}
 
 	public function testall_as_listResultsFallBackToDiskWhenMemoryIsNotPresent()
@@ -175,15 +175,15 @@ class TestRevisionable extends ModelTestCase
 		$this->populate();
 
 		// Setup both the memory and disk cache.
-		$result = ProgrammeSetting::all_as_list();
+		$result = Programme::all_as_list();
 
 		// Wipe the memory cache.
-		ProgrammeSetting::$list_cache = false;
+		Programme::$list_cache = false;
 
 		// Check we still have the disk cache
-		$this->assertTrue(Cache::has('ProgrammeSetting--options-list'), 'Somehow we wiped the disk cache when we wiped the memory cache.');
+		$this->assertTrue(Cache::has('Programme--options-list'), 'Somehow we wiped the disk cache when we wiped the memory cache.');
 
-		$this->assertEquals($result, ProgrammeSetting::all_as_list());
+		$this->assertEquals($result, Programme::all_as_list());
 	}
 
 }
