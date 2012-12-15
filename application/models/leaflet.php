86ce5eaa8d3caa2128f0abd77f486769286516bd
thybag <thybag@gmail.com>
2012-12-15 16:58:54 +0000

    Add name constraint back to leaflet model (is actually only used by test as simpleadmin will always overwrite this)

--- application/models/leaflet.php
+++ application/models/leaflet.php
@@ -2,8 +2,8 @@
 class Leaflet extends SimpleData
 {
 	public static $rules = array(
+			'name'  => 'required|unique:leaflets|max:255',
             'campus'  => 'required|exists:campuses,id',
             'tracking_code'  => 'required|url'
     );
-    
 }
