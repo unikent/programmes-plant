f15ffaa6da1a8b603f91ebd4c4039da0b51a45a2
Matthew Bull <bull.matthew@gmail.com>
2013-01-03 20:32:49 +0000

    Fixed api.test.php so it won't break in Travis
    * removed extra call to tearDown() because this was unnecessary and was causing the Travis error
    * not now removing all directories and files from the api storage folder. The dummy modules json files are now kept.
    * added some comments
    * converted tabs to spaces

--- application/tests/controllers/api.test.php
+++ application/tests/controllers/api.test.php
@@ -3,305 +3,304 @@ require_once dirname(dirname(__FILE__)) . '/lib/ControllerTestCase.php';
 
 class TestAPI_Controller extends ControllerTestCase
 {
-	public $input = false;
+    public $input = false;
 
-	public function recursively_delete_directory($dir) 
-	{
-		$files = array_diff(scandir($dir), array('.','..'));
+    public function recursively_delete_directory($dir) 
+    {
+        $files = array_diff(scandir($dir), array('.','..'));
 
-	    foreach ($files as $file) {
-	      (is_dir("$dir/$file")) ? static::recursively_delete_directory("$dir/$file") : unlink("$dir/$file");
-	    }
+        foreach ($files as $file) {
+            if ( is_dir("$dir/$file") )
+            {
+                static::recursively_delete_directory("$dir/$file");
+            }
+            // don't delete the dummy module json files if they're there
+            elseif ( ! strstr($file, '_modules_test.json') )
+            {
+                unlink("$dir/$file");
+            }
+        }
 
-		return rmdir($dir);
-	}
+        return true;
+    }
 
-	public function populate($input)
-	{
-		Programme::create($input)->save();
-	}
+    public function populate($input)
+    {
+        Programme::create($input)->save();
+    }
 
-	public static function setUpBeforeClass()
-	{
-		Tests\Helper::migrate();
+    public static function setUpBeforeClass()
+    {
+        Tests\Helper::migrate();
+    }
 
-		// Remove all Programmes
-		TestAPI_Controller::tearDown();
-	}
+    public static function tearDownAfterClass()
+    {
+        // Ugly way to get our .gitignore back.
+        // Travis CI will likely hate this.
+        exec("git checkout storage/api/.gitignore");
+    }
 
-	public static function tearDownAfterClass()
-	{
-		// Ugly way to get our .gitignore back.
-		// Travis CI will likely hate this.
-		exec("git checkout storage/api/.gitignore");
-	}
+    public function tearDown()
+    {
+        $programmes = Programme::all();
 
-	public function tearDown()
-	{
-		$programmes = Programme::all();
+        foreach ($programmes as $programme)
+        {
+            $programme->delete();
+        }
 
-		foreach ($programmes as $programme)
-		{
-			$programme->delete();
-		}
+        $programme_revisions = ProgrammeRevision::all();
 
-		$programme_revisions = ProgrammeRevision::all();
+        foreach ($programme_revisions as $revision)
+        {
+            $revision->delete();
+        }
 
-		foreach ($programme_revisions as $revision)
-		{
-			$revision->delete();
-		}
+        $global_settings = GlobalSetting::all();
 
-		$global_settings = GlobalSetting::all();
+        foreach ($global_settings as $setting)
+        {
+            $setting->delete();
+        }
 
-		foreach ($global_settings as $setting)
-		{
-			$setting->delete();
-		}
+        $programme_settings = ProgrammeSetting::all();
 
-		$programme_settings = ProgrammeSetting::all();
+        foreach ($programme_settings as $setting)
+        {
+            $setting->delete();
+        }
 
-		foreach ($programme_settings as $setting)
-		{
-			$setting->delete();
-		}
+        // We need to reset our API cache somehow.
+        // This also deletes our .gitignore, which we restore after the tests are done.
+        if (file_exists(path('storage') . 'api/') && is_dir(path('storage') . 'api/'))
+        {
+            static::recursively_delete_directory(path('storage') . 'api/');
+        }
 
-		// We need to reset our API cache somehow.
-		// This also deletes our .gitignore, which we restore after the tests are done.
-		if (file_exists(path('storage') . 'api/') && is_dir(path('storage') . 'api/'))
-		{
-			static::recursively_delete_directory(path('storage') . 'api/');
-		}
+        parent::tearDown();
+    }
 
-		parent::tearDown();
-	}
+    public function generate_programme_dependancies(){
+        ProgrammeField::create(
+                array(
+                        'field_name' => 'New field',
+                        'field_type' => 'textarea',
+                        'prefill' => 0,
+                        'active' => 1,
+                        'view' => 1
+                    )
+            )->save();
 
-	public function generate_programme_dependancies(){
-		ProgrammeField::create(
-				array(
-						'field_name' => 'New field',
-						'field_type' => 'textarea',
-						'prefill' => 0,
-						'active' => 1,
-						'view' => 1
-					)
-			)->save();
+        Award::create(
+                array(
+                        'name' => 'Hello Award',
+                        'longname' => 'Long hello award'
+                    )
+            )->save();
 
-		Award::create(
-				array(
-						'name' => 'Hello Award',
-						'longname' => 'Long hello award'
-					)
-			)->save();
+        Campus::create(
+                array(
+                        'name' => 'Hello Campus'
+                    )
+            )->save();
 
-		Campus::create(
-				array(
-						'name' => 'Hello Campus'
-					)
-			)->save();
+        Faculty::create(
+                array(
+                        'name' => 'Hello Faculty'
+                    )
+            )->save();
 
-		Faculty::create(
-				array(
-						'name' => 'Hello Faculty'
-					)
-			)->save();
+        Leaflet::create(
+                array(
+                        'name' => 'Hello Leaflet'
+                    )
+            )->save();
 
-		Leaflet::create(
-				array(
-						'name' => 'Hello Leaflet'
-					)
-			)->save();
+        School::create(
+                array(
+                        'name' => 'Hello School',
+                        'faculties_id' => 1
+                    )
+            )->save();
 
-		School::create(
-				array(
-						'name' => 'Hello School',
-						'faculties_id' => 1
-					)
-			)->save();
+        Subject::create(
+                array(
+                        'name' => 'Hello Subject'
+                    )
+            )->save();
 
-		Subject::create(
-				array(
-						'name' => 'Hello Subject'
-					)
-			)->save();
+        ProgrammeSetting::create(
+                array(
+                        'id' => 1,
+                        'year' => '2014'
+                    )
+            )->save();
+        $ps = ProgrammeSetting::find(1);
+        $revisions = $ps->get_revisions('selected');
+        $ps->makeRevisionLive($revisions[0]);
 
-		ProgrammeSetting::create(
-				array(
-						'id' => 1,
-						'year' => '2012'
-					)
-			)->save();
-		$ps = ProgrammeSetting::find(1);
-		$revisions = $ps->get_revisions('selected');
-		$ps->makeRevisionLive($revisions[0]);
+        GlobalSetting::create(
+                array(
+                        'id' => 1,
+                        'year' => '2014'
+                    )
+            )->save();
+        $gs = GlobalSetting::find(1);
+        $revisions = $gs->get_revisions('selected');
+        $gs->makeRevisionLive($revisions[0]);
+    }
 
-		GlobalSetting::create(
-				array(
-						'id' => 1,
-						'year' => '2012'
-					)
-			)->save();
-		$gs = GlobalSetting::find(1);
-		$revisions = $gs->get_revisions('selected');
-		$gs->makeRevisionLive($revisions[0]);
-	}
+    public function create_programme($input = false)
+    {
+        if (! $input)
+        {
+            $input = array(
+                'id' => 1, 
+                Programme::get_title_field() => 'Programme 1',
+                'year' => '2014'
+            );
+        }
 
-	public function create_programme($input = false)
-	{
-	    if (! $input)
-	    {
-    		$input = array(
-    			'id' => 1, 
-    			Programme::get_title_field() => 'Programme 1',
-    			'year' => '2012'
-    		);
-		}
+        $this->populate($input);
 
-		$this->populate($input);
+        $course = Programme::find($input['id']);
+        return $course;
+    }
 
-		$course = Programme::find($input['id']);
-		return $course;
-	}
+    public function make_programme_live($id = 1)
+    {
+        $course = Programme::find($id);
+        
+        if(!empty($course))
+        {
+            $revisions = $course->get_revisions('selected');
 
-	public function make_programme_live($id = 1)
-	{
-		$course = Programme::find($id);
-		
-		if(!empty($course))
-		{
-			$revisions = $course->get_revisions('selected');
+            if (isset($revisions[0])) {
+                $course->makeRevisionLive($revisions[0]);
+            }
+            return $course;
+        }
+        
+        return null;
+    }
 
-			if (isset($revisions[0])) {
-				$course->makeRevisionLive($revisions[0]);
-			}
-			return $course;
-		}
-		
-		return null;
-	}
+    public function testget_indexReturnsHTTPCode204WithNoDataCached()
+    {
+        $response = $this->get('api@index', array('2014', 'ug'));
+        $this->assertEquals('204', $response->status());
+    }
 
-	public function testget_indexReturnsHTTPCode204WithNoDataCached()
-	{
-		$response = $this->get('api@index', array('2012', 'ug'));
-		$this->assertEquals('204', $response->status());
-	}
+    public function testget_indexReturnsHTTPCode200WithDataCached()
+    {
+        $this->generate_programme_dependancies();
 
-	public function testget_indexReturnsHTTPCode200WithDataCached()
-	{
-		$this->generate_programme_dependancies();
+        $course = $this->create_programme();
+        $course = $this->make_programme_live($course->id);
+    
+        $response = $this->get('api@index', array($course->year, 'ug'));
+        $this->assertEquals('200', $response->status());
+    }
 
-		$course = $this->create_programme();
-		$course = $this->make_programme_live($course->id);
-	
-		$response = $this->get('api@index', array($course->year, 'ug'));
-		$this->assertEquals('200', $response->status());
-	}
+    public function testget_indexReturnsJSONWithData()
+    {
+        $this->generate_programme_dependancies();
+        
+        $input = array(
+            'id' => 1, 
+            'programme_title_1' => 'Programme 1',
+            'year' => '2014',
+            'programme_suspended_53' => '',
+            'programme_withdrawn_54' => '',
+            );
 
-	public function testget_indexReturnsJSONWithData()
-	{
-		$this->generate_programme_dependancies();
-		
-		$input = array(
-    		'id' => 1, 
-    		'programme_title_1' => 'Programme 1',
-    		'year' => '2012',
-    		'programme_suspended_53' => '',
-    		'programme_withdrawn_54' => '',
-    		);
+        $course = $this->create_programme($input);
+        $course = $this->make_programme_live($input['id']);
 
-		$course = $this->create_programme($input);
-		$course = $this->make_programme_live($input['id']);
+        $response = $this->get('api@index', array($input['year'], 'ug'));
+        $returned_data = json_decode($response->render());
+        
+        $returned_data = $returned_data->$input['id'];
+        
+        $this->assertEquals($input['id'], $returned_data->id);
+        $this->assertEquals($input['programme_title_1'], $returned_data->name);
+    }
+    
+    public function testget_indexReturnsJSONWithSuspendedWithdrawnData()
+    {
+        $this->generate_programme_dependancies();
+        
+        $input = array(
+            'id' => 1, 
+            'programme_title_1' => 'Programme 1',
+            'year' => '2014',
+            'programme_suspended_53' => '',
+            'programme_withdrawn_54' => ''
+        );
 
-		$response = $this->get('api@index', array($input['year'], 'ug'));
-		$returned_data = json_decode($response->render());
-		
-		//currently this produces an error because our index returns nothing.
-		//it seems to be a bug with sqlite that it doesnt like the query in the 
-		//revisionable model that generates the index feed
-		$returned_data = $returned_data->$input['id'];
-		
-		$this->assertEquals($input['id'], $returned_data->id);
-		$this->assertEquals($input['programme_title_1'], $returned_data->name);
-	}
-	
-	public function testget_indexReturnsJSONWithSuspendedWithdrawnData()
-	{
-		$this->generate_programme_dependancies();
-		
-		$input = array(
-    		'id' => 1, 
-    		'programme_title_1' => 'Programme 1',
-    		'year' => '2012',
-    		'programme_suspended_53' => '',
-    		'programme_withdrawn_54' => ''
-		);
+        $course = $this->create_programme($input);
+        $course = $this->make_programme_live($input['id']);
 
-		$course = $this->create_programme($input);
-		$course = $this->make_programme_live($input['id']);
+        $response = $this->get('api@index', array($input['year'], 'ug'));
+        $returned_data = json_decode($response->render());
+        
+        $returned_data = $returned_data->$input['id'];
+        
+        $this->assertEquals($input['id'], $returned_data->id);
+        $this->assertEquals($input['programme_title_1'], $returned_data->name);
+    }
 
-		$response = $this->get('api@index', array($input['year'], 'ug'));
-		$returned_data = json_decode($response->render());
-		
-		//currently this produces an error because our index returns nothing.
-		//it seems to be a bug with sqlite that it doesnt like the query in the 
-		//revisionable model that generates the index feed
-		$returned_data = $returned_data->$input['id'];
-		
-		$this->assertEquals($input['id'], $returned_data->id);
-		$this->assertEquals($input['programme_title_1'], $returned_data->name);
-	}
+    public function testget_programmeReturns204WithNoCache()
+    {
+        $input = array(
+            'id' => 1, 
+            'programme_title_1' => 'Programme 1',
+            'year' => '2014',
+        );
 
-	public function testget_programmeReturns204WithNoCache()
-	{
-    	$input = array(
-    		'id' => 1, 
-    		'programme_title_1' => 'Programme 1',
-    		'year' => '2012',
-		);
+        $course = $this->create_programme($input);
+        $course = $this->make_programme_live($input['id']);
 
-		$course = $this->create_programme($input);
-		$course = $this->make_programme_live($input['id']);
+        $response = $this->get('api@programme', array($course->year, 'ug', $course->id));
 
-		$response = $this->get('api@programme', array($course->year, 'ug', $course->id));
+        $this->assertEquals('204', $response->status());
+    }
 
-		$this->assertEquals('204', $response->status());
-	}
+    public function testget_programmeReturns200WhenCachePresent()
+    {
+        $this->generate_programme_dependancies();
 
-	public function testget_programmeReturns200WhenCachePresent()
-	{
-		$this->generate_programme_dependancies();
+        $input = array(
+            'id' => 1, 
+            'programme_title_1' => 'Programme 1',
+            'year' => '2014',
+        );
 
-		$input = array(
-    		'id' => 1, 
-    		'programme_title_1' => 'Programme 1',
-    		'year' => '2012',
-		);
+        $course = $this->create_programme($input);
+        $course = $this->make_programme_live($input['id']);
 
-		$course = $this->create_programme($input);
-		$course = $this->make_programme_live($input['id']);
+        $response = $this->get('api@programme', array($course->year, 'ug', $course->id));
 
-		$response = $this->get('api@programme', array($course->year, 'ug', $course->id));
+        $this->assertEquals('200', $response->status());
+    }
 
-		$this->assertEquals('200', $response->status());
-	}
+    public function testget_programmeReturnsJSONWhenCachePresent()
+    {
+        $this->generate_programme_dependancies();
 
-	public function testget_programmeReturnsJSONWhenCachePresent()
-	{
-		$this->generate_programme_dependancies();
+        $input = array(
+            'id' => 1, 
+            'programme_title_1' => 'Programme 1',
+            'year' => '2014',
+        );
+        
+        $course = $this->create_programme($input);
+        $course = $this->make_programme_live($input['id']);
 
-		$input = array(
-    		'id' => 1, 
-    		'programme_title_1' => 'Programme 1',
-    		'year' => '2012',
-		);
-
-		$course = $this->create_programme($input);
-		$course = $this->make_programme_live($input['id']);
-
-		$response = $this->get('api@programme', array($course->year, 'ug', $course->id));
-		$returned_data = json_decode($response->render());
-		
-		$this->assertEquals($input['programme_title_1'], $returned_data->programme_title);
-		//perhaps some more assertions needed here
-	}
+        $response = $this->get('api@programme', array($course->year, 'ug', $course->id));
+        $returned_data = json_decode($response->render());
+        
+        $this->assertEquals($input['programme_title_1'], $returned_data->programme_title);
+        //perhaps some more assertions needed here
+    }
 }
