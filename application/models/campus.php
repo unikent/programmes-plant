e46975d9bcb4cc79e57de5f3dc8e4e6000cc24bf
conatus <alex@recordsonribs.com>
2013-01-04 16:26:26 +0000

    Convert To Tabs

--- application/models/campus.php
+++ application/models/campus.php
@@ -1,16 +1,16 @@
 <?php
-class Campus extends SimpleData
-{
+class Campus extends SimpleData {
+	
 	public static $rules = array(
-    	'name' => 'required|unique:campuses|max:255',
-    	'identifier' => 'numeric',
-    	'address_1' => 'required',
-    	'address_2' => 'required',
-    	'address_3' => 'required',
-    	'email' => 'email',
-    	'phone' => 'required|match:/^([0-9 ]+)$/',
-    	'postcode' => 'required',
-    	'url' => 'url'
-    );
-    
+		'name' => 'required|unique:campuses|max:255',
+		'identifier' => 'numeric',
+		'address_1' => 'required',
+		'address_2' => 'required',
+		'address_3' => 'required',
+		'email' => 'email',
+		'phone' => 'required|match:/^([0-9 ]+)$/',
+		'postcode' => 'required',
+		'url' => 'url'
+	);
+	
 }
