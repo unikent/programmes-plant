c042cf65a510ffb9a97fe9afbda4308c814065cd
conatus <alex@recordsonribs.com>
2013-01-04 16:45:55 +0000

    Address 3 And Telephone Not Required, Telephone Number Extended
    - Phone number can now have pluses, hyphens and brackets.
    - Address 3 no longer required.
    - Telephone no longer required.

--- application/models/campus.php
+++ application/models/campus.php
@@ -1,14 +1,13 @@
 <?php
 class Campus extends SimpleData {
-	
+
 	public static $rules = array(
 		'name' => 'required|unique:campuses|max:255',
 		'identifier' => 'numeric',
 		'address_1' => 'required',
 		'address_2' => 'required',
-		'address_3' => 'required',
 		'email' => 'email',
-		'phone' => 'required|match:/^([0-9 ]+)$/',
+		'phone' => 'match:/^([0-9 \-+\(\)])/',
 		'postcode' => 'required',
 		'url' => 'url'
 	);
