5bf56f334a66b4922489eca25fe834bf4a83bf93
thybag <thybag@gmail.com>
2013-01-09 13:39:32 +0000

    Change data structure (add migration)

---
+++ application/migrations/2013_01_09_123456_add_more_revision_data.php
@@ -0,0 +1,91 @@
+<?php
+
+class Add_more_revision_data {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    Schema::table('programmes', function($table){
+			$table->drop_column('published_by');
+		});
+		Schema::table('programme_settings', function($table){
+			$table->drop_column('published_by');
+		});
+		Schema::table('global_settings', function($table){
+			$table->drop_column('published_by');
+		});
+
+		Schema::table('programmes_revisions', function($table){
+			$table->drop_column('created_by');
+			$table->drop_column('published_by');
+		});
+		Schema::table('programmes_revisions', function($table){
+			$table->string('edits_by');
+			$table->string('published_by');
+			$table->date('published_at');
+		});
+		Schema::table('programme_settings_revisions', function($table){
+			$table->drop_column('created_by');
+		});
+		Schema::table('programme_settings_revisions', function($table){
+			$table->string('edits_by');
+			$table->string('published_by');
+			$table->date('published_at');
+		});
+		Schema::table('global_settings_revisions', function($table){
+			$table->drop_column('created_by');
+		});
+		Schema::table('global_settings_revisions', function($table){
+			$table->string('edits_by');
+			$table->string('published_by');
+			$table->date('published_at');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    	Schema::table('programmes', function($table){
+			$table->string('published_by');
+		});
+		Schema::table('programme_settings', function($table){
+			$table->string('published_by');
+		});
+		Schema::table('global_settings', function($table){
+			$table->string('published_by');
+		});
+
+		Schema::table('programmes_revisions', function($table){
+			$table->string('created_by');
+			$table->drop_column('edits_by');
+			$table->drop_column('published_by');
+			$table->drop_column('published_at');
+		
+		});
+		Schema::table('programmes_revisions', function($table){
+				$table->integer('published_by');
+		});
+		Schema::table('programme_settings_revisions', function($table){
+			$table->string('created_by');
+			$table->drop_column('edits_by');
+			$table->drop_column('published_by');
+			$table->drop_column('published_at');
+		});
+		Schema::table('global_settings_revisions', function($table){
+			$table->string('created_by');
+			$table->drop_column('edits_by');
+			$table->drop_column('published_by');
+			$table->drop_column('published_at');
+		});
+
+	}
+
+}
