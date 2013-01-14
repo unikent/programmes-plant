298b0c923a80fb615e31eccdf2894c058fcdb336
Matthew Bull <bull.matthew@gmail.com>
2013-01-14 22:24:51 +0000

    Minor changes to wysiwyg.
    * fixed default hieght/width and added resize button
    * added list button
    * fixed format button
    
    Also tidied up the display of the overridable field marker

--- public/js/kent-texteditor.js
+++ public/js/kent-texteditor.js
@@ -1,21 +1,24 @@
 
- var ck_config = {
-	    toolbar: [
-			    	{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'RemoveFormat' ] },
-			    	{ name: 'styles', items: [ 'Format' ] },
-					{ name: 'links', items: [ 'Link', 'Unlink', 'Mailto' ] },
-					[ 'Scayt'], //spell checker?
-					[ 'Source'],
-					[ 'Undo', 'Redo' ],	
-					{ name: 'document', items: ['Maximize'] },																	
-				],
-			removePlugins : 'elementspath',
-			resize_enabled : false,
-		    customConfig: '',
-			language: 'en-gb'
-		};
+var ck_config = {
+	toolbar: [
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'BulletedList' ] },
+		{ name: 'styles', items: [ 'Format' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Mailto' ] },
+		[ 'Scayt'], // spell checker
+		[ 'Source'],
+		[ 'Undo', 'Redo' ],
+		{ name: 'document', items: ['Maximize'] },
+	],
+	resize_enabled : false,
+	customConfig: '',
+	language: 'en-gb',
+	width: "600px",
+	height: "300px",
+	format_tags: 'p;h2;h3;h4;h5;h6',
+	resize_enabled: true
+};
 
- $('textarea').each( function(){
- 
+$('textarea').each( function(){
 	CKEDITOR.replace( $(this)[0], ck_config);
 });
