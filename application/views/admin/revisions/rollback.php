c9e931dc0c6a5660929f5ee2a12c2536e9ef3e2c
thybag <thybag@gmail.com>
2013-01-17 09:35:24 +0000

    Move rollback options to own page.

--- application/views/admin/revisions/rollback.php
+++ application/views/admin/revisions/rollback.php
@@ -1,28 +1,30 @@
 <div style='padding:10px;height:30px;' class='alert <?php if($programme->live=='2'):?>alert-success<?php else:?>alert-info<?php endif;?> alert-block'>    
   <div style='float:right;'>
+    <a class="btn btn-warning" href="<?php echo  action(URI::segment(1).'/'.URI::segment(2).'/'.URI::segment(3).'@edit', array($programme->id))?>" >View active revisions</a>
     <a class="btn btn-info" href="<?php echo  action(URI::segment(1).'/'.URI::segment(2).'/'.URI::segment(3).'@edit', array($programme->id))?>" >Return to edit form</a>
   </div>
 </div>
 
 <h1><?php echo $programme->{Programme::get_title_field()}; ?><?php echo isset($programme->award->name) ? ' - <em>'.$programme->award->name.'</em>' : '' ; ?></h1>
 
-<h3>Active revisions</h3>
+<h3>Roll live revision back</h3>
+<p>Warning. Using this feature will change the revision published on the live website.</p>
 
 <?php
 // Loop through revisions (display modes for active & previous are differnt)
-
+$active_r = true;
 foreach ($revisions as $revision){
-
-    echo View::make('admin.revisions.partials.active_revision', array('revision' => $revision, 'programme' => $programme))->render();
-    //After live switch mode to "non-active"
-    if($revision->status =='live'){
-      break;
-    }
-
+  if($active_r){
+      //After live switch mode to "non-active"
+      if($revision->status =='live'){
+        $active_r=false;
+      }
+  }else{
+     echo View::make('admin.revisions.partials.previous_revision', array('revision' => $revision, 'programme' => $programme))->render();
+  }
 }
 ?>
 
-<a class="btn btn-danger" href="<?php echo  action(URI::segment(1).'/'.URI::segment(2).'/'.URI::segment(3).'@rollback', array($programme->id))?>" >Emergency rollback options</a>
 <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
 
 <div class="modal hide fade" id="make_revision_live">
