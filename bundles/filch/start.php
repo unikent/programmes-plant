8638cb5bed218263242f0a1c00b2ac2eb13dbf92
thybag <thybag@gmail.com>
2013-01-21 11:27:30 +0000

    Make filch play nice with php 5.3 (no nice [] array syntax :( )

--- bundles/filch/start.php
+++ bundles/filch/start.php
@@ -1,8 +1,8 @@
 <?php
 
-Autoloader::map([
+Autoloader::map(array(
 	'Filch\\Cache' => __DIR__.DS.'cache'.EXT,
-]);
+));
 
 Cache::extend('filch', function(){
 	return new Filch\Cache(path('storage').'cache'.DS);
