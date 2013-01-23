07a96dea643b9504fb6b24947c3ef197f070f34c
=thybag <thybag@gmail.com>
2013-01-23 17:21:07 +0000

    Add static caching to programme settings

--- application/models/programmesetting.php
+++ application/models/programmesetting.php
@@ -5,6 +5,8 @@ class ProgrammeSetting extends Revisionable
     public static $table = 'programme_settings';
     public static $revision_model = 'ProgrammeSettingRevision';
     protected $data_type_id = 'programme_setting';
+
+    public static $settings_cache = array();
     
     /**
      * Get the default setting for the specified column
@@ -16,13 +18,19 @@ class ProgrammeSetting extends Revisionable
      */
     public static function get_setting($year, $colname)
     {
-    	$settings = ProgrammeSettingRevision::where('year', '=', $year)->where('status', '=', 'live')->get($colname);
-    	
-    	if(!empty($settings[0])){
-    		return $settings[0]->$colname;
-    	}
-    	
-    	return null;
+
+        // If in memoery cache, return from there
+        if(isset(static::$settings_cache[$year])) return static::$settings_cache[$year]->$colname;
+
+        // Otherwise, get data
+    	$settings = ProgrammeSettingRevision::where('year', '=', $year)->where('status', '=', 'live')->first();
+        
+        // Check data was okay
+    	if(!$settings) return null;
+
+        // Store in memoery cache & return
+        static::$settings_cache[$year] = $settings->to_array();
+    	return $settings->$colname; 	
     }
 
 }
