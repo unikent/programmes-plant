5b5d84876f0f1986c5eb6c2c6626ba6209648dfe
=thybag <thybag@gmail.com>
2013-01-29 09:53:15 +0000

    Actually fix the bug mentioned before (missed this part of commit)

--- application/views/admin/programmes/form.php
+++ application/views/admin/programmes/form.php
@@ -34,7 +34,7 @@
 <?php echo View::make('admin.inc.partials.formfields', array('year' => $year,'sections' => $sections, 'programme' => isset($programme) ? $programme : null,'create'=>($create && !$clone), 'from' => 'programmes'))->render(); ?>
 
 
-<?php echo Form::actions('programmes', $programme); ?>
+<?php echo Form::actions('programmes', isset($programme) ? $programme : null) ?>
 
 <?php echo Form::close(); ?>
 
