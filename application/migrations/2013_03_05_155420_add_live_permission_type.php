17d1d3f07cd676ab4a7376067e66d6d1f6edb041
conatus <alex@recordsonribs.com>
2013-03-05 16:24:34 +0000

    Add migration that adds the additional roles needed by making live

---
+++ application/migrations/2013_03_05_155420_add_live_permission_type.php
@@ -0,0 +1,48 @@
+<?php
+require_once Bundle::path('verify_ldap') . '../verify/models/EloquentVerifyBase.php';
+require_once Bundle::path('verify_ldap') . '../verify/models/Permission.php';
+
+use \Verify\Models\Permission as Permission;
+
+class Add_Live_Permission_Type {
+
+	private $permissions = array(
+		'make_programme_live',
+		'submit_programme_for_editing'
+	);
+
+	public function __construct()
+	{
+		Config::set('verify::verify.prefix', 'usersys');
+		Config::set('verify::verify.super_admin', 'Administrator');
+	}
+
+	/**
+	 * Add additional permission types.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		foreach($this->permissions as $permission_name)
+		{
+			$permission = Permission::create(array('name' => $permission_name));
+			$permission->save();
+		}
+	}
+
+	/**
+	 * Remove additional permission types.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		foreach($this->permissions as $permission_name)
+		{
+			$permission = Permission::where('name', '=', $permission_name)->first();
+			$permission->delete();
+		}
+	}
+
+}
