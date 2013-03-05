0a4db1b8bd4302373295bd1474d3b25358a69ee8
thybag <thybag@gmail.com>
2013-03-05 11:34:12 +0000

    More spelling issues

--- application/views/admin/inc/no_permissions.php
+++ application/views/admin/inc/no_permissions.php
@@ -1,6 +1,6 @@
 <h1>Insufficient privileges</h1>
 <h4>Your account does not have the permissions required to access the current page</h4>
-<p> Please contact an administrator if you belive you are seeing this message in error.</p>
+<p> Please contact an administrator if you believe you are seeing this message in error.</p>
 
 
 <h5>Required permissions</h4>
