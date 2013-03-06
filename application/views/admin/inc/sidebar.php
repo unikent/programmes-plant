a8d0435337353612e0e31fc65f5e2b82e329aca7
conatus <alex@recordsonribs.com>
2013-03-06 17:42:20 +0000

    Add editing inbox to the sidebar

--- application/views/admin/inc/sidebar.php
+++ application/views/admin/inc/sidebar.php
@@ -10,7 +10,10 @@
 	
 	if (URI::segment(2) == 'fields') $selectedType = URI::segment(1);
   ?>
-
+        <?php if (Auth::user()->can("recieve_edit_requests")): ?>
+            <li class="nav-header">Editing</li>
+            <li class="<?php echo ( (URI::segment(1) == 'editor_inbox' ) ? 'active' : false )?>"><a href="<?php echo url('editor_inbox')?>"><i class="icon-inbox"></i> Inbox</a></li>
+        <?php endif; ?>
 
         <li class="nav-header">Core Data</li>
 
