da87af5637e5a9065425043f1f891c8ce4970b3d
conatus <alex@recordsonribs.com>
2013-03-06 14:54:41 +0000

    Neathen up get_make_live

--- application/controllers/revisionable.php
+++ application/controllers/revisionable.php
@@ -84,22 +84,22 @@ class Revisionable_Controller extends Admin_Controller {
 	/**
 	 * Routing for GET /$year/$type/$object_id/make_live/$revision_id
 	 * Routing for GET /$year/$type/make_live/$revision_id
+	 * 
+	 * revisionable_item_id id can be ommited, resulting in that varible containg 
+	 * the revision id instead. (Object assumed to have id 1 in this case)
 	 *
-	 * @param int    $year         The year of the programme (not used, but to keep routing happy).
-	 * @param string $type         The type, either undegrad/postgrade (not used, but to keep routing happy)
+	 * @param int    $year                  The year of the programme (not used, but to keep routing happy).
+	 * @param string $type                  The type, either undegrad/postgrade (not used, but to keep routing happy)
 	 * @param int    $revisionable_item_id  The object ID we are pushing a revision live on.
-	 * @param int    $revision_id  The revision ID we are putting live.
-	 *
-	 * @note revisionable_item_id id can be ommited, resulting in that varible containg 
-	 *	the revision id instead. (Object assumed to have id 1 in this case)
+	 * @param int    $revision_id           The revision ID we are putting live.
 	 */
 	public function get_make_live($year, $type, $revisionable_item_id = false, $revision_id = false)
 	{
-
 		// Check to see we have what is required.
 		$data = $this->get_revision_data($revisionable_item_id, $revision_id);
+
 		// If somthing went wrong
-		if(!$data) return Redirect::to($year.'/'.$type.'/'.$this->views);
+		if (!$data) return Redirect::to($year.'/'.$type.'/'.$this->views);
 
 		// Get data & make revision live
 		list($item, $revision) = $data;
