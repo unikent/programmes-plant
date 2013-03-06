c2b0ecd5aadac8dc98163acda29a3b43c07372be
conatus <alex@recordsonribs.com>
2013-03-06 15:13:51 +0000

    Abstract out insufficient permissions message

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -20,5 +20,16 @@ class Admin_Controller extends Base_Controller {
 		// Construct parent.
 		parent::__construct();
 	}
+
+	/**
+	 * Show a message when the user has insufficient permissions to perform a particular action.
+	 * 
+	 * @param array|string  $permissions  Single permissions string or array of permissions the user doesn't have, but needs to perform the action.
+	 * @return View         A view telling the user about the error message.
+	 */
+	protected function insufficient_permissions($permissions)
+	{
+		return $this->layout->nest('content', 'admin.inc.no_permissions', array("perms" => (array)$permissions));
+	}
 	
 }
