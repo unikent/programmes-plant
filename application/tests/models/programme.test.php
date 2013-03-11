63f559c38dde954e12f5a2b06c424e2af73a3a39
conatus <alex@recordsonribs.com>
2013-03-11 12:40:02 +0000

    Test for get_under_review

--- application/tests/models/programme.test.php
+++ application/tests/models/programme.test.php
@@ -2,8 +2,24 @@
 
 class TestProgramme extends ModelTestCase {
 
+	public function tearDown()
+	{
+		$this->clear_models(array('ProgrammeRevision', 'Programme'));
+	}
+
 	public function testXcrifyReturnsAStdClass() {}
 
 	public function testXcrifyReturnsAValidFlattenedVersionOfTheObject() {}
+
+	public function testget_under_reviewReturnsRevisionsUnderReview()
+	{
+		$revision = ProgrammeRevision::create(array('status' => 'under_review'));
+		$revision->save();
+
+		$under_review = Programme::get_under_review();
+
+		$this->assertCount(1, $under_review);
+		$this->assertTrue(is_a($under_review[0], 'ProgrammeRevision'));
+	}
 
 }
