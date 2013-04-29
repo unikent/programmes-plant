3ef96539a03e9acd98e245c5158c2ad1f5c42c67
Matthew Bull <bull.matthew@gmail.com>
2013-04-29 11:46:30 +0200

    updated email text in language file

--- application/language/en/emails.php
+++ application/language/en/emails.php
@@ -4,13 +4,17 @@
 	/**
 	 * emails to users
 	 */
-	'admin_notification' => ':author has submitted a new programme update for :title, which is currently pending approval.',
+	'admin_notification' => ":author has submitted a new programme update for :title, which is currently <a href='https://webtools.kent.ac.uk/programmes/editor/inbox'>pending approval</a>.",
 	
 	/**
 	 * emails from users
 	 */
 	'user_notification' => array(
-		'approve' => 'Dear :author, \n\nYour update to :title has now been approved. \n\nMany thanks!',
+		'approve' => "Dear :author,\n\n
+				Regarding your recent updates to <a href='https://webtools.kent.ac.uk/programmes/2014/ug/programmes/edit/:id'>:title</a> on the programmes-plant: these have now been approved and will shortly appear on the live website at <a href='http://www.kent.ac.uk/courses/undergraduate/:id/:slug'>http://www.kent.ac.uk/courses/undergraduate/:id/:slug</a>\n\n
+				Feel free to reply to this email if you have any questions about your updates.\n\n
+				Regards,\n
+				EMS Publishing Office\n\n",
 		),
 		
 	);
