c7cc7059043c2e1ad3c432bf740625e63688c6af
Mark Fendley <m.s.fendley@kent.ac.uk>
2013-05-01 11:58:52 +0200

    Update emails.php
    Changed ```<br>``` to ```<br />```

--- application/language/en/emails.php
+++ application/language/en/emails.php
@@ -13,11 +13,11 @@ return array(
 	 */
 	'user_notification' => array(
 								'approve' => array('title' => "programme-plant updates approved",
-													'body' => "Dear :author,<br><br>
-												Regarding your recent updates to <a href='https://webtools.kent.ac.uk/programmes/2014/ug/programmes/edit/:id'>:title</a> on the programmes-plant: these have now been approved and will shortly appear on the live website at <a href='http://www.kent.ac.uk/courses/undergraduate/:id/:slug'>http://www.kent.ac.uk/courses/undergraduate/:id/:slug</a><br><br>
-												Feel free to reply to this email if you have any questions about your updates.<br><br>
-												Regards,<br><br>
-												EMS Publishing Office<br><br>"
+													'body' => "Dear :author,<br /><br />
+												Regarding your recent updates to <a href='https://webtools.kent.ac.uk/programmes/2014/ug/programmes/edit/:id'>:title</a> on the programmes-plant: these have now been approved and will shortly appear on the live website at <a href='http://www.kent.ac.uk/courses/undergraduate/:id/:slug'>http://www.kent.ac.uk/courses/undergraduate/:id/:slug</a><br /><br />
+												Feel free to reply to this email if you have any questions about your updates.<br /><br />
+												Regards,<br /><br />
+												EMS Publishing Office<br /><br />"
 											),
 								'request' => array('title' => "RE: Your updates to :title"),
 							),
