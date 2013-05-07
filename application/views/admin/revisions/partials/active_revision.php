e995ca5267d84c9be85079aa9448d037413fd004
gnozu <bull.matthew@gmail.com>
2013-05-07 10:51:14 +0100

    removed redundant message on publish revision info bar on the revisions listing page

--- application/views/admin/revisions/partials/active_revision.php
+++ application/views/admin/revisions/partials/active_revision.php
@@ -13,7 +13,6 @@
 
 		
 		<?php echo $revision->get_identifier_string() ?> <br/>
-		<?php echo __('revisions.status_live_published_text', array('date' => $revision->get_published_time(), 'user' => $revision->made_live_by)) ?>
 		<?php if(get_class($programme) == 'Programme'):?>
 			<div style='float:right;'><a class="popup_toggler" href='#unpublish' rel="<?php echo $unpublish_link;?>"><?php echo __("revisions.unpublish"); ?></a> </div>
 		<?php endif?>
