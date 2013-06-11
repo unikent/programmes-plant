711c4089ade9675d438bfe407359529fc6bbbf9b
thybag <thybag@gmail.com>
2013-06-11 14:05:14 +0100

    view revision

--- application/controllers/revisionable.php
+++ application/controllers/revisionable.php
@@ -234,7 +234,7 @@ class Revisionable_Controller extends Admin_Controller {
 		if(!$revision) return Redirect::to($year.'/'.$type.'/'.$this->views);
 
 		$data = array();
-		$data['attributes'] = Programme::get_attributes_list();
+		$data['attributes'] = $model::get_attributes_list();
 		$data['revision'] = $revision;
 		$data['programme'] = $course;
 
