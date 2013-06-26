b1237dc85994b04840e47bb24a9d9eb7faa436de
thybag <thybag@gmail.com>
2013-06-26 11:45:38 +0100

    Disable deliveries of none super users

--- application/views/admin/programmes/deliveries.php
+++ application/views/admin/programmes/deliveries.php
@@ -10,9 +10,11 @@
 
 <?php
 
-	if(!Auth::user()->can("view_pg_deliveries")) die();
+	//if(!Auth::user()->can("view_pg_deliveries")) die();
 
 	$disabled = !Auth::user()->can("edit_pg_deliveries");
+
+	$field_state = ($disabled) ? array('disabled'=>'disabled') : array();
 
 ?>
 
@@ -27,36 +29,41 @@
 		</tr>
 
 		<?php foreach($deliveries as $delivery): ?>
-			
-				<tr>
-					<form action="" method="post">
-						<td> <?php echo Form::select('award', PG_Award::all_as_list(), $delivery->award); ?> </td>
-						<td> <?php echo Form::text('pos_code', $delivery->pos_code); ?> </td>
-						<td> <?php echo Form::text('mcr', $delivery->mcr); ?>  </td>
-						<td> <?php echo Form::select('attendance_pattern',array('full-time'=>'Full time', 'part-time'=> 'Part time'),$delivery->attendance_pattern); ?></td>
-						<td>
-						<?php if($disabled):?>
-						 	<input type="hidden" name="id" value="<?php echo $delivery->id; ?>" />
-							<input type='submit' class='btn btn-primary' value='Save' /> 
-							<a href='#' class='btn btn-danger' href=''>Remove</a>
-						<?php endif;?>
-						</td>
-					</form>
-				</tr>
-			
+			<tr>
+				<form action="" method="post">
+					<td> <?php echo Form::select('award', PG_Award::all_as_list(), $delivery->award, $field_state); ?> </td>
+					<td> <?php echo Form::text('pos_code', $delivery->pos_code, $field_state); ?> </td>
+					<td> <?php echo Form::text('mcr', $delivery->mcr, $field_state); ?>  </td>
+					<td> <?php echo Form::select('attendance_pattern',array('full-time'=>'Full time', 'part-time'=> 'Part time'),$delivery->attendance_pattern, $field_state); ?></td>
+					<td>
+					<?php if(!$disabled):?>
+					 	<input type="hidden" name="id" value="<?php echo $delivery->id; ?>" />
+						<input type='submit' class='btn btn-primary' value='Save' /> 
+						<a href='#' class='btn btn-danger' href=''>Remove</a>
+					<?php endif;?>
+					</td>
+				</form>
+			</tr>
 		<?php endforeach; ?>
-		<?php if($disabled):?>
-		<tr>
-			<form action="" method="post">
-				<td> <?php echo Form::select('award', PG_Award::all_as_list()); ?> </td>
-				<td> <?php echo Form::text('pos_code'); ?> </td>
-				<td> <?php echo Form::text('mcr'); ?> </td>
-				<td> <?php echo Form::select('attendance_pattern',array('full-time'=>'Full time', 'part-time'=> 'Part time'),''); ?></td>
-				<td><input type='submit' class='btn btn-success' value='create' /></td>
-			</form>
-			<?php endif;?>
-		</tr>
+		<?php if(!$disabled):?>
+			<tr>
+				<form action="" method="post">
+					<td> <?php echo Form::select('award', PG_Award::all_as_list()); ?> </td>
+					<td> <?php echo Form::text('pos_code'); ?> </td>
+					<td> <?php echo Form::text('mcr'); ?> </td>
+					<td> <?php echo Form::select('attendance_pattern', array('full-time'=>'Full time', 'part-time'=> 'Part time')); ?></td>
+					<td><input type='submit' class='btn btn-success' value='create' /></td>
+				</form>
+			</tr>
+		<?php endif;?>
 	</table>
+	<?php if($disabled):?>
+		<div class='container-fluid'>
+			<div class='span12 alert alert-info'>
+				<p>Please contact EMS if you need any of these details to be updated</p>
+			</div>
+		</div>
+	<?php endif; ?>
 </div>
 </body>
 </html>
