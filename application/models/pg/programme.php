decf1fcd91350613286ee6e80698744383f72ad7
Justice Addison <jay@tisqo.com>
2013-08-17 08:35:10 +0100

    added a few functions to get pg awards. they are now showing in edit view for pg course. addresses #563

--- application/models/pg/programme.php
+++ application/models/pg/programme.php
@@ -16,4 +16,35 @@ class PG_Programme extends Programme {
 	  	return $this->has_many('pg_deliveries', 'programme_id');
 	}
 
+	/**
+	 * Get this programme's awards.
+	 * 
+	 * @return Award The award for this programme.
+	 */
+	public function awards()
+	{
+		$award_field = static::get_award_field();
+		$ids = explode(',', $this->$award_field);
+		return PG_Award::where_in('id', $ids)->get();
+	}
+
+	/**
+	 * Get a list of this programme's award names.
+	 * 
+	 * @return string A comma seperated string of awards.
+	 */
+	public function get_award_names()
+	{
+		$awards = $this->awards();
+		$award_string = '';
+		$count = 0;
+
+		foreach ($awards as $award) {
+			$award_string .= (($count > 0) ? ', ' : '') . $award->name;
+			$count++;
+		}
+
+		return $award_string;
+	}
+
 }
