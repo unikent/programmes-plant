2a284e2969f2091605601c1768449dad448d9a18
Matthew Bull <m.bull@kent.ac.uk>
2013-10-14 14:34:26 +0100

    added task to regenerate UG module session data for 2014
    also fixes moduledata task in line with making the UG module_session field an overridable field

---
+++ application/tasks/module-session.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('max_execution_time', 600);
+
+require_once path('base') . 'vendor/autoload.php';
+
+class Module_Session_Task {
+
+	/**
+	 * run
+	 */
+	public function run($arguments = array())
+	{
+		Auth::login(1);
+		$programmes = UG_Programme::where('year', '=', '2014')->get();
+		foreach ($programmes as $programme)
+		{
+			$live_id = $programme->live_revision;
+			$current_id = $programme->current_revision;
+			$live_revision = $programme->find_live_revision();
+			if ( $live_revision && ($live_id == $current_id) )
+			{
+				$programme->module_session_86 = '';
+				$programme->save();
+				$new_current_id = $programme->current_revision;
+				$programme->make_revision_live((int) $new_current_id);
+			}
+		}
+	}
+
+
+}
