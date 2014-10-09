ccc5fd99f1881145686655f02057dddb66aca197
jna-unikent <jna@kent.ac.uk>
2014-10-09 13:00:19 +0100

    add a departments column to user table and pull it in from ldap

---
+++ application/migrations/2014_10_09_111504_add_department_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+class Add_Department_To_Users {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('usersys_users', function($table)
+		{
+			$table->string('department', 255);
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('usersys_users', function($table)
+		{
+			$table->drop_column('department');
+		});
+	}
+
+}
