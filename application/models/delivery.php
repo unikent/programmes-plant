101bcedbd0bdffd206f85119344a43ed986f1844
jna-unikent <jna@kent.ac.uk>
2014-12-03 15:44:46 +0000

    re-arrange things

--- application/models/delivery.php
+++ application/models/delivery.php
@@ -30,32 +30,26 @@ abstract class Delivery extends SimpleData
 		return $this->belongs_to(static::$level . "_award", 'award');
 	}
 
-	/**
-	 * generate API data (overides copy in simpledata to avoid use of "Hidden")
-	 * Get live version of API data from database
-	 *
-	 * @param year (Unused - PHP requires signature not to change)
-	 * @param data (Unused - PHP requires signature not to change)
-	 */
-	public static function generate_api_data($year = false, $data = false)
-	{
-		// keys
-		$model = strtolower(get_called_class());
-		$cache_key = 'api-'.$model;
+	// Generate deliveries cache
+	protected function generate_cache(){
+		$programme_model = static::$level . '_programme';
 		
-		// make data
-		$data = array();
-		foreach (static::get() as $record) {
-			// Direct grab of attributes is faster than to_array 
-			// since don't need to worry about realtions & things like that
-			$data[$record->attributes["id"]] = $record->attributes;
-		}
-		
-		// Store data in to cache
-		Cache::put($cache_key, $data, 2628000);
-		
-		// return
-		return $data;
+		// Find instance id & year
+		$p = $programme_model::where('id','=',$this->programme_id)->first(array('id','instance_id','year'));
+
+		// Generate cache
+		static::generate_programme_deliveries($p->instance_id, $p->year, $p->id);
+
+		//forget output cache
+		$level = static::$level;
+		Cache::forget("api-output-{$level}/programme-{$p->year}-{$p->instance_id}");
+	}
+
+	// get data from cache (or make a new one)
+	public static function get_programme_deliveries($iid, $year){
+		$level = static::$level;
+		$key = "{$level}-deliveries.{$iid}-{$year}";
+		return (Cache::has($key)) ? Cache::get($key) : static::generate_programme_deliveries($iid, $year); 
 	}
 
 	// generate new cache data
@@ -85,29 +79,32 @@ abstract class Delivery extends SimpleData
 		return $cacheable;
 	}
 
-	// Generate deliveries cache
-	protected function generate_cache(){
-		$programme_model = static::$level . '_programme';
+	/**
+	 * generate API data (overides copy in simpledata to avoid use of "Hidden")
+	 * Get live version of API data from database
+	 *
+	 * @param year (Unused - PHP requires signature not to change)
+	 * @param data (Unused - PHP requires signature not to change)
+	 */
+	public static function generate_api_data($year = false, $data = false)
+	{
+		// keys
+		$model = strtolower(get_called_class());
+		$cache_key = 'api-'.$model;
 		
-		// Find instance id & year
-		$p = $programme_model::where('id','=',$this->programme_id)->first(array('id','instance_id','year'));
-
-		// Generate cache
-		static::generate_programme_deliveries($p->instance_id, $p->year, $p->id);
-
-		//forget output cache
-		$level = static::$level;
-		Cache::forget("api-output-{$level}/programme-{$p->year}-{$p->instance_id}");
+		// make data
+		$data = array();
+		foreach (static::get() as $record) {
+			// Direct grab of attributes is faster than to_array 
+			// since don't need to worry about realtions & things like that
+			$data[$record->attributes["id"]] = $record->attributes;
+		}
+		
+		// Store data in to cache
+		Cache::put($cache_key, $data, 2628000);
+		
+		// return
+		return $data;
 	}
-
-	// get data from cache (or make a new one)
-	public static function get_programme_deliveries($iid, $year){
-		$level = static::$level;
-		$key = "{$level}-deliveries.{$iid}-{$year}";
-		return (Cache::has($key)) ? Cache::get($key) : static::generate_programme_deliveries($iid, $year); 
-	}
-
-
-
 
 }
