3e27767aba011f4a004cef3975e4d84d56f61233
thybag <thybag@gmail.com>
2016-11-29 16:57:38 +0000

    Use Simple_Admin_Controller routing methods

--- application/controllers/profiles.php
+++ application/controllers/profiles.php
@@ -2,21 +2,10 @@
 class Profiles_Controller extends Simple_Admin_Controller {
 	public $restful = true;
 	public $views = 'profile';
-	public $model = '';
-	public $level = null;
+	public $model = 'Profile';
 	public $custom_form = true;
 	public $shared_data = false;
 
-	// Determine correct model (PG / UG)
-	public function __construct()
-	{
-
-		$this->level = URI::segment(1);
-		$this->model = ($this->level=='ug') ? 'UG_Profile' : 'PG_Profile';
-
-		// Construct parent.
-		parent::__construct();
-	}
 
 	/**
 	 * Return all data and send to an index view.
@@ -24,7 +13,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 	public function get_index()
 	{
 		$model = $this->model;
-	//	$this->data['shared'] = $this->shared_data;
+		$this->data['shared'] = $this->shared_data;
 		$this->data['items'] = $model::all_active('course')->get();
 		$this->layout->nest('content', 'admin.profile.index', $this->data);
 	}
@@ -35,6 +24,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 	public function post_create()
 	{
 		$model = $this->model;
+		$url = $this->get_base_page();
 
 		$rules = array(
 			'name' => 'required|max:255'
@@ -44,7 +34,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 		{
 			Messages::add('error', $model::$validation->errors->all());
 			Input::flash();//Save previous inputs to avoid blanking form.
-			return Redirect::to($this->level . '/' . $this->views.'/create')->with_input();
+			return Redirect::to($url.'/create')->with_input();
 		}
 
 		$cats = is_array(Input::get('subject_categories')) ? implode(',', array_filter(Input::get('subject_categories'))) : '';
@@ -59,7 +49,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 
 		Messages::add('success', __($this->l . 'success.create'));
 
-		return Redirect::to($this->level . '/' . $this->views.'');
+		return Redirect::to($url);
 	}
 
 	/**
@@ -68,6 +58,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 	public function post_edit()
 	{
 		$model = $this->model;
+		$url = $this->get_base_page();
 
 		$rules = array(
 			'id'  => 'required|exists:'. $model::$table .',id',
@@ -78,7 +69,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 		{
 			Messages::add('error', $model::$validation->errors->all());
 			Input::flash();//Save previous inputs to avoid blanking form.
-			return Redirect::to($this->level . '/' . $this->views.'/edit/'.Input::get('id'));
+			return Redirect::to($url . '/edit/' . Input::get('id'));
 		}
 
 		$cats = is_array(Input::get('subject_categories')) ? implode(',', array_filter(Input::get('subject_categories'))) : '';
@@ -91,7 +82,7 @@ class Profiles_Controller extends Simple_Admin_Controller {
 		$update->save();
 
 		Messages::add('success', __($this->l . 'success.edit'));
-		return Redirect::to($this->level . '/' . $this->views.'');
+		return Redirect::to($url);
 	}
 
 }
