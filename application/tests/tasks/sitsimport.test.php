88175c4cbf8fd23104ab9c7242d9fc9a3b42b07e
Justice Addison <j.n.addison@kent.ac.uk>
2018-06-20 15:59:30 +0100

    update data

--- application/tests/tasks/sitsimport.test.php
+++ application/tests/tasks/sitsimport.test.php
@@ -4,17 +4,18 @@ require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/tasks/sitsimport.php'
 
 class TestSITSImport_Task extends PHPUnit_Framework_TestCase {
 
-	private $validUGCourse = array( 'progID' => '394UG' );
-	private $validPGCourse = array( 'progID' => '71PG' );
+	private $validUGCourse = array( 'pp_prospectus' => 'UG', 'pp_id' => '394' );
+	private $validPGCourse = array( 'pp_prospectus' => 'PG', 'pp_id' => '71' );
 
 	private $fullCourse = array (
-		'progID' => '174PG'
-		, 'award' => 'Master of Science'
-		, 'mcr' => 'PAAS000101MS-FD'
+		'pp_prospectus' => 'PG'
+		, 'pp_id' => '174'
+		, 'pp_award_id_pg' => '1'
+		, 'mcr_code' => 'PAAS000101MS-FD'
 		, 'pos_code' => 'ACTSCIAP:MSC-T1'
 		, 'ari_code' => 'MCR000000013'
-		, 'description' => 'Applied Actuarial Science - MSc - full-time at Canterbury'
-		, 'attendance_pattern' => 'full-time');
+		, 'mcr_name' => 'Applied Actuarial Science - MSc - full-time at Canterbury'
+		, 'attendance_mode' => 'FT');
 
 	private $task;
 
@@ -35,7 +36,7 @@ class TestSITSImport_Task extends PHPUnit_Framework_TestCase {
 		$this->assertTrue( is_numeric( $currPGYear ) );
 	}
 
-	public function testCourseLevelIsDetectedFromProgID() {
+	public function testCourseLevelIsDetectedFromDelivery() {
 
 		$ugCourse = $this->validUGCourse;
 		$pgCourse = $this->validPGCourse;
