32bd58df20bcd752913b60d70d61619d0f579a99
Christian Cable <christiancable@gmail.com>
2020-02-05 11:19:02 +0000

    KentStandards formatting - no code change

--- application/tasks/resizethumbnails.php
+++ application/tasks/resizethumbnails.php
@@ -3,7 +3,8 @@
  *
  *
  */
-class ResizeThumbnails_Task {
+class ResizeThumbnails_Task
+{
 
 	/**
 	 * Run the setinitaluser task
@@ -12,34 +13,34 @@ class ResizeThumbnails_Task {
 	 */
 	public function run($arguments = array())
 	{
-		if(sizeof($arguments) === 0){
+		if (sizeof($arguments) === 0) {
 			$maxWidth =(int)Config::get('images.thumbnail_max_width');
 		} else {
 			$maxWidth = (int)$arguments[0];
 		}
-		if($maxWidth < 50) {
+		if ($maxWidth < 50) {
 			echo "\n Error: width cannot be less than 50. \n";
-            die();
+			die();
 		}
 		echo "Resizing images to maximum width of $maxWidth\n\n";
 		$images = Image::get();
 
-		foreach( Image::get() as $image) {
+		foreach (Image::get() as $image) {
 			$path = Config::get('images.image_directory', path('storage').'images');
 			$src = $path . '/' . $image->id . '.jpg';
 			$dest = $path . '/' . $image->id . '_thumb.jpg';
 			echo "Resizing {$image->id} - {$image->name}...";
-			if( $this->make_thumb( $path.'/'.$image->id.'.jpg', $path.'/'.$image->id.'_thumb.jpg', $maxWidth) ) {
+			if ($this->make_thumb($path.'/'.$image->id.'.jpg', $path.'/'.$image->id.'_thumb.jpg', $maxWidth)) {
 				echo "SUCCESS!\n";
-			}
-			else {
+			} else {
 				echo "FAILED\n";
 			}
 			flush();
 		}
 	}
 
-	protected function make_thumb($src, $dest, $desired_width) {
+	protected function make_thumb($src, $dest, $desired_width)
+	{
 		$result = false;
 		/* read the source image */
 		$source_image = imagecreatefromjpeg($src);
@@ -49,12 +50,12 @@ class ResizeThumbnails_Task {
 		// Get thumb sizeing
 		list($desired_height, $desired_width) = Image::getThumbSize($desired_width, $width, $height);
 
-		if($source_image) {
+		if ($source_image) {
 			/* create a new, "virtual" image */
 			$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
-			if($virtual_image) {
+			if ($virtual_image) {
 				/* copy source image at a resized size */
-				if( imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height)) {
+				if (imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height)) {
 					/* create the physical thumbnail image to its destination */
 					$result = imagejpeg($virtual_image, $dest);
 				}
@@ -65,4 +66,3 @@ class ResizeThumbnails_Task {
 		return $result;
 	}
 }
-
