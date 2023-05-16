a7a4f783e894cc17788500cd957f2c4a09bc3f36
Christian Cable <c.f.cable@kent.ac.uk>
2023-05-16 12:24:38 +0100

    rename config file to local, a place for local settings

--- application/views/admin/inc/partials/retirement_banner.php
+++ application/views/admin/inc/partials/retirement_banner.php
@@ -1,10 +1,10 @@
-<?php if (Config::get('kent.ug_banner_html') && 'ug' == (URLParams::$type)): ?>
+<?php if (Config::get('local.ug_banner_html') && 'ug' == (URLParams::$type)): ?>
     <div class="alert alert-error" role="alert">
-        <?php if (Config::get('kent.ug_banner_heading')): ?>
-            <h2 class="alert-heading"><?= Config::get('kent.ug_banner_heading') ?></h2>
+        <?php if (Config::get('local.ug_banner_heading')): ?>
+            <h2 class="alert-heading"><?= Config::get('local.ug_banner_heading') ?></h2>
         <?php endif; ?>
         <p>
-            <?= Config::get('kent.ug_banner_html') ?>
+            <?= Config::get('local.ug_banner_html') ?>
         </p>
     </div>
 <?php endif; ?>
