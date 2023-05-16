6eb67e2f46852268079bd0c1e9d75f1dd9cdfdec
Christian Cable <c.f.cable@kent.ac.uk>
2023-05-16 12:18:48 +0100

    move the call to the partial to the main admin layout

--- application/views/admin/programmes/index.php
+++ application/views/admin/programmes/index.php
@@ -1,4 +1,3 @@
-<?php echo View::make('admin.inc.partials.retirement_banner')->render(); ?>
 <h1><?php echo View::make('admin.inc.partials.type_marker')->render(); ?>Programmes</h1>
 <p style="margin-top:20px; margin-bottom:20px"><?php echo  __('programmes.' . URI::segment(2) . '_introduction', array('year' => URI::segment(1))) ?></p>
 
